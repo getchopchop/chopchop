d92953c9e607fbded3094904e7d6643cfa333037
Dave Redfern <dave@daveredfern.com>
2015-06-11 10:25:33 +0100

    Sorting out folders

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,6 @@
 
 		var $body = $('body');
 
-
 		// Vars for toggle
 		var toggle = '.js-toggle',
 			toggleTitle = '.js-toggle__title',
@@ -12,7 +11,7 @@
 
 		// Hide all toggles
 		$(toggleContent).hide();
-	
+
 		// When clicked, do class toggling of active
 		$(toggleTitle).on('click', function(e) {
 			e.preventDefault();
@@ -24,13 +23,12 @@
 				$(this).parents(toggle).removeClass('active');
 			}
 		});
-	
-	
-	
+
+
 		// Active menu magic
 		var navHover = '.nav--primary > ul > li';
 		var timeoutInt;
-	
+
 		$(navHover).hover(function() {
 			$(navHover).removeClass('active');
 			$(this).addClass('active');
@@ -41,21 +39,22 @@
 		}, function() {
 			var self = this;
 			hideOverlay();
-		
+
 			if(timeoutInt) {
 				window.clearTimeout(timeoutInt);
 			}
-		
+
 			timeoutInt = window.setTimeout(function() {
 				$(self).removeClass('active');
 			}, 500);
 		});
-		
+
+
 		// burger
 		var burgerItem = '.nav__burger',
 			burgerMenu = '.nav--burger',
 			burgerState = 0;
-			
+
 		$(burgerItem).click(function() {
 			if(burgerState === 0) {
 				burgerState = 1;
@@ -67,7 +66,7 @@
 				hideOverlay();
 			}
 		});
-		
+
 		enquire.register("screen and (min-width:52em)", {
 			match : function() {
 		    	if(burgerState == 1) {
@@ -75,21 +74,24 @@
 		    	}
 		    }
 		});
-	
+
+
 		// Search focus
 		var searchFocus = '.search input',
 			searchSuggestions = '.suggestive-search';
-	
+
 		$(searchFocus).on('focus', function() {
-			showOverlay('overlay-search');
+			if($(this).parents('.search--invert')) {
+				showOverlay('overlay-search');
+			}
 			$(searchSuggestions).show();
 		});
 		$(searchFocus).on('focusout', function() {
 			hideOverlay();
 			$(searchSuggestions).hide();
 		});
-	
-	
+
+
 		// Show and hide overlays
 		function showOverlay(type) {
 			$('body').addClass(type);
@@ -103,6 +105,6 @@
 				$('body').removeClass(prefix + types.join(' ' + prefix));
 			});
 		}
-	
+
 	});
 }(jQuery));
