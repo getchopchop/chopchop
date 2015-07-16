26694883325c65909c8670ef1f19ee67ddef85f9
Gareth Joyce <gareth@Gareths-MacBook-Pro.local>
2015-07-16 16:56:06 +0100

    intro to primer page

---
+++ public_html/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="mobile-web-app-capable" content="yes">
+
+	<title>Primer - Little patterns to get us going</title>
+
+	<?php include('../src/templates/_styles.php'); ?>
+
+</head>
+
+<body>
+    
+    <?php include('../src/templates/primer/header.php'); ?>
+    
+    <div class="container">
+        <h1>Welcome to Primer</h1>
+        <p>Primer is a simple library of common reuseable patterns we use across all projects everyday.</p>
+        
+        <h2>How primer came about</h2>
+        <p>Mention <a href="http://patternlab.io/">Brad Frost</a></p>
+        
+        <h3>The aims of primer</h3>
+        <ul>
+            <li>Cut down the amount of repetitive work</li>
+        </ul>
+        
+        <h3>What primer is not</h3>
+        <ul>
+            <li>A complete framework</li>
+        </ul>
+    </div>
+
+</body>
+</html>
--- src/templates/primer/header.php
+++ src/templates/primer/header.php
@@ -1,5 +1,16 @@
 <header class="primer-header">
     <div class="container">
-        <a href="/" class="primer-logo">Primer</a>
+        <a href="/" class="primer-logo">&rho; primer</a>
+        
+        <nav class="nav nav--primary primer-nav">
+            <ul>
+                <li><a href="atoms.php">Atoms</a></li>
+                <li><a href="molecules.php">Molecules</a></li>
+                <li><a href="style-guide.php">Style guide</a></li>
+                <li><a href="page.php">CMS page</a></li>
+                <li><a href="listing.php">Product listing</a></li>
+            </ul>
+        </nav>
+        
     </div>
 </header>
