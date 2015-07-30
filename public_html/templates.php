677afabdbc8096c4dfdc8d4b90b1f323357a3ef7
Gareth Joyce <gareth@Gareths-MacBook-Pro.local>
2015-07-30 15:59:49 +0100

    more minor tweaks, removed direct descendant in li as was too heavy

---
+++ public_html/templates.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="mobile-web-app-capable" content="yes">
+
+	<title>Primer - Templates</title>
+
+	<?php include( '../src/templates/_styles.php'); ?>
+
+</head>
+
+<body>
+    
+    <?php include('../src/templates/primer/header.php'); ?>
+    What is going here?
+
+</body>
+
+</html>
--- src/templates/primer/header.php
+++ src/templates/primer/header.php
@@ -2,8 +2,8 @@
     <div class="container">
         <a href="/" class="primer-logo">&rho; primer</a>
         
-        <nav class="nav">
-            <ul class="list-unstyled">
+        <nav class="nav nav--stack">
+            <ul>
                 <li><a href="helpers.php">Helpers</a></li>
                 <li><a href="atoms.php">Atoms</a></li>
                 <li><a href="molecules.php">Molecules</a></li>
