ef48710eb7063c06cd4fdb0e72386734cb82de10
Dave Redfern <dave@daveredfern.com>
2015-07-30 16:18:40 +0100

    Adding mixin for grid system so we can build custom ones

--- src/templates/primer/header.php
+++ src/templates/primer/header.php
@@ -1,7 +1,7 @@
 <header class="primer-header">
     <div class="container">
         <a href="/" class="primer-logo">&rho; primer</a>
-        
+
         <nav class="nav">
             <ul class="list-unstyled">
                 <li><a href="atoms.php">Atoms</a></li>
@@ -9,8 +9,9 @@
                 <li><a href="organisms.php">Organisms</a></li>
                 <li><a href="templates.php">Templates</a></li>
                 <li><a href="pages.php">Pages</a></li>
+                <li><a href="layout.php">Layout</a></li>
             </ul>
         </nav>
-        
+
     </div>
 </header>
