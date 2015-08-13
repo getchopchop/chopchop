471d514271a73413c83b276dec302535c0ef5f7b
Dave Redfern <dave@daveredfern.com>
2015-08-13 14:35:30 +0100

    Fixing links to scripts and styles

--- public_html/templates/_js.php
+++ public_html/templates/_js.php
@@ -1,4 +1,4 @@
 <!-- Template: _js !-->
-<script src="js/vendor/jquery.min.js"></script>
-<script src="js/script.min.js"></script>
+<script src="vendor/jquery.min.js"></script>
+<script src="build/js/script.min.js"></script>
 <!-- End template: _js !-->
