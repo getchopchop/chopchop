a56c04442e3d2fa64f6fdacc36c4cbe364320c1d
Dave Redfern <dave@daveredfern.com>
2015-08-27 14:44:43 +0100

    Docs: documentation setup

---
+++ templates/getting-started/structure.php
@@ -0,0 +1,30 @@
+# Structure
+
+## Global folder structure
+
+### Source
+
+### Build (temporary folder)
+
+### Node Modules (temporary folder)
+
+### Bower Components (temporary folder)
+
+
+## Sass folder structure
+
+### Helpers
+
+The helper folder contains mixins only. None of the Sass contained in this folder creates CSS, it's there to help you create CSS. An example of helpers include grids, triangles, breakpoint.
+
+### Utilities
+
+Utilities are a collection of classes or Sass extends that can be used to achieve single tasks. Examples of utilities include visibility, alignments and clearfix classes.
+
+### Atoms
+
+### Molecules
+
+### Organisms
+
+### Templates
