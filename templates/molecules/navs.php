14c3f02ca415852d3cf71e2942ca945697facf16
Gareth <gjoyce@iweb.co.uk>
2015-09-10 16:11:51 +0100

    Molecules: added multilevel navigation.

--- templates/molecules/navs.php
+++ templates/molecules/navs.php
@@ -33,16 +33,81 @@
     <div class="container">
         
         <h2>Multi Level Primary Nav</h2>
-        <p>The Primary Nav pattern is generally used as the main navigation for the website.</p>
+        <p>The Multi Level Primary Nav pattern is generally used as the main navigation for the website.</p>
 
         <div class="chop-example">
             
             <nav class="nav--primary">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
+
+                <ol>
+                    <li class="level0 nav-1 first parent">
+                        <a href="lighting.html" class="level0 has-children">Level 0</a>
+                        <ul class="level0">
+                            <li class="level1">
+                                <a class="level1" href="lighting.html">Level 1</a>
+                            </li>
+                            <li class="level1 nav-1-1 first parent">
+                                <a href="lighting/floodlight.html" class="level1 has-children">Level 1</a>
+                                <ul class="level1">
+                                    <li class="level2">
+                                        <a class="level2" href="lighting/floodlight.html">Level 2</a>
+                                    </li>
+                                    <li class="level2 nav-1-1-1 first parent">
+                                        <a href="lighting/floodlight/cree-led.html" class="level2 has-children">Level 2</a>
+                                        <ul class="level2">
+                                            <li class="level3">
+                                                <a class="level3" href="lighting/floodlight/cree-led.html">Level 3</a>
+                                            </li>
+                                            <li class="level3 nav-1-1-1-1 first last parent">
+                                                <a href="lighting/floodlight/cree-led/site-light.html" class="level3 has-children">Level 3</a>
+                                                <ul class="level3">
+                                                    <li class="level4">
+                                                        <a class="level4" href="lighting/floodlight/cree-led/site-light.html">Level 4</a>
+                                                    </li>
+                                                    <li class="level4 nav-1-1-1-1-1 first last"><a href="lighting/floodlight/cree-led/site-light/rechargeable.html" class="level4 ">Level 4</a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="level2 nav-1-1-2 parent">
+                                        <a href="lighting/floodlight/metal-halide.html" class="level2 has-children">Level 2</a>
+                                        <ul class="level2">
+                                            <li class="level3">
+                                                <a class="level3" href="lighting/floodlight/metal-halide.html">Level 3</a>
+                                            </li>
+                                            <li class="level3 nav-1-1-2-1 first last">
+                                                <a href="lighting/floodlight/metal-halide/site-light.html" class="level3 ">Level 3</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="level2 nav-1-1-3 parent">
+                                        <a href="lighting/floodlight/led.html" class="level2 has-children">Level 2</a>
+                                        <ul class="level2">
+                                            <li class="level3">
+                                                <a class="level3" href="lighting/floodlight/led.html">Level 3</a>
+                                            </li>
+                                            <li class="level3 nav-1-1-3-1 first last">
+                                                <a href="lighting/floodlight/led/site-light.html" class="level3 ">Level 3</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="level2 nav-1-1-4 last parent">
+                                        <a href="lighting/floodlight/halogen.html" class="level2 has-children">Level 2</a>
+                                        <ul class="level2">
+                                            <li class="level3">
+                                                <a class="level3" href="lighting/floodlight/halogen.html">Level 3</a>
+                                            </li>
+                                            <li class="level3 nav-1-1-4-1 first last"><a href="lighting/floodlight/halogen/site-light.html" class="level3 ">Level 3</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ol>
+                
             </nav>
             
         </div>
