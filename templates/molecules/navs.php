288cedc2d05922a69f1ea83bad484640beba333c
Gareth <gjoyce@iweb.co.uk>
2015-09-10 15:51:47 +0100

    Molecules: navs, primary added and multi level started, needs finishing.

--- templates/molecules/navs.php
+++ templates/molecules/navs.php
@@ -1,8 +1,73 @@
 <div class="well">
     <div class="container">
         
-        <h2>Stacked Nav</h2>
+        <h2>Primary Nav</h2>
+        <p>The Primary Nav pattern is generally used as the main navigation for the website.</p>
+
+        <div class="chop-example">
+            <nav class="nav--primary">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        {% highlight code %}
+<nav class="nav--primary">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+</nav>
+        {% end highlight code %}
         
+    </div>
+</div>
+
+
+
+<div class="well">
+    <div class="container">
+        
+        <h2>Multi Level Primary Nav</h2>
+        <p>The Primary Nav pattern is generally used as the main navigation for the website.</p>
+
+        <div class="chop-example">
+            
+            <nav class="nav--primary">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </nav>
+            
+        </div>
+
+        {% highlight code %}
+<nav class="nav--primary">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+</nav>
+        {% end highlight code %}
+        
+    </div>
+</div>
+
+
+
+<div class="well">
+    <div class="container">
+        
+        <h2>Stacked Nav</h2>
+        <p>The Stacked Nav pattern is generally used to style a list of links that would appear in a tall container such as a sidebar or <a href="/molecules/drawers">drawer</a>.
+            
         <div class="chop-example">
             <nav class="nav--stack">
                 <ul>
