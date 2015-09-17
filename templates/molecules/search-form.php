4a3962aa2b02a841cceab00dfab5cce8ed8596b9
Gareth <gjoyce@iweb.co.uk>
2015-09-17 11:33:48 +0100

    this is wrong and I will fix now.

--- templates/molecules/search-form.php
+++ templates/molecules/search-form.php
@@ -1,5 +1,13 @@
-<form action="#" method="post" class="form-search">
-    <label class="is-vishidden">Search the site</label>     
-    <input type="search" placeholder="Search the site" />
-    <input type="submit" class="btn search-submit" value="Search">
-</form>
+<div class="container">
+    
+    
+    <p>This pattern doesn't neccessarily need to be a search form, here we are just showing an example of the <a href="/atoms/forms#inline-form">inline form</a> pattern in use as a search form.</p>
+    
+    <form action="#" method="post" class="form-inline form-search">
+        <label class="is-vishidden">Search the site</label>     
+        <input type="search" placeholder="Search the site" class="form-inline__query" />
+        <input type="submit" class="btn search-submit" value="Search">
+    </form>
+    
+    {% %}
+</div>
