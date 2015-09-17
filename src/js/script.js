e81a012e2f83f820af85818ba8ad6922ef2aab61
Gareth <gjoyce@iweb.co.uk>
2015-09-17 10:20:26 +0100

    Molecules: added skip links.

--- src/js/script.js
+++ src/js/script.js
@@ -1,75 +1,117 @@
 ;(function($){
     $(document).ready(function() {
         
+        
         // =============================================
         // Sensible Variables
         // =============================================
+        
         var $body = $('body');
-
-
+        
+        
+        
         // =============================================
         // Hamburger Toggle Class
         // =============================================
+        
         var $hamburgers = $('.c-hamburger');
         
         $hamburgers.on('click', function(e) {
             e.preventDefault();
             $(this).toggleClass('is-active');
         });
-
-
+        
+        
+        
         // =============================================
         // Accordion to Tabs
         // =============================================
-		var tabLinkSelector = ".tabs__tab",
-			tabContentSelector = ".tabs__content",
-			tabContainerSelector = ".tabs",
-			activeLinkClass = "tabs__tab--active",
-			activeContentClass = "tabs__content--active";
+        
+        var tabLinkSelector = ".tabs__tab",
+            tabContentSelector = ".tabs__content",
+            tabContainerSelector = ".tabs",
+            activeLinkClass = "tabs__tab--active",
+            activeContentClass = "tabs__content--active";
 
-		$(tabContainerSelector).each(function(){
-			var $this = $(this);
-				$links = $this.find(tabLinkSelector);
-		
-			$this.find(tabContentSelector).removeClass(activeContentClass);
-			$this.find(tabLinkSelector).removeClass(activeLinkClass);
-	
-			if($links.size()){
-				var $firstLink = $($links[0]);
-				$this.find(tabLinkSelector + "[href='" + $firstLink.attr('href') + "']").addClass(activeLinkClass);
-				$this.find($firstLink.attr('href')).addClass(activeContentClass);
-			}
-		});
+        $(tabContainerSelector).each(function(){
+            var $this = $(this);
+                $links = $this.find(tabLinkSelector);
+                
+            $this.find(tabContentSelector).removeClass(activeContentClass);
+            $this.find(tabLinkSelector).removeClass(activeLinkClass);
+            
+            if($links.size()){
+                var $firstLink = $($links[0]);
+                $this.find(tabLinkSelector + "[href='" + $firstLink.attr('href') + "']").addClass(activeLinkClass);
+                $this.find($firstLink.attr('href')).addClass(activeContentClass);
+            }
+        });
 
-		$(tabLinkSelector).on('click.tabs', function(e){
-			e.preventDefault();
-	
-			var $this = $(this),
-				$container = $(this).closest(tabContainerSelector);
-				$content = $container.find($this.attr('href'));
+        $(tabLinkSelector).on('click.tabs', function(e){
+            e.preventDefault();
+            
+            var $this = $(this),
+                $container = $(this).closest(tabContainerSelector);
+                $content = $container.find($this.attr('href'));
+                
+            if(!$content.size()){
+            	return;
+            }
 
-				if(!$content.size()){
-					return;
-				}
-
-			$container.find(tabLinkSelector).removeClass(activeLinkClass);
-			$container.find(tabContentSelector).removeClass(activeContentClass);
-			$content.addClass(activeContentClass);
-			$container.find(tabLinkSelector + "[href='" + $this.attr('href') + "']").addClass(activeLinkClass);
-		});
+            $container.find(tabLinkSelector).removeClass(activeLinkClass);
+            $container.find(tabContentSelector).removeClass(activeContentClass);
+            $content.addClass(activeContentClass);
+            $container.find(tabLinkSelector + "[href='" + $this.attr('href') + "']").addClass(activeLinkClass);
+        });
         
         
         
-        // Toggle Blocks
+        // =============================================
+        // Single Toggle
+        // =============================================
+        
         $.fn.toggleSingle = function() {
             return this.on('click', function(e) {
                 e.preventDefault();
-                $(this).next().toggleClass('active');
+                $(this).next().toggleClass('no-display');
             });
-        }
+        };
         $('.block-toggle').toggleSingle();
-  
-
+        
+        
+        
+        // =============================================
+        // Skip Links
+        // =============================================
+        
+        var skipContents = $('.skip-content');
+        var skipLinks = $('.skip-link');
+        
+        skipLinks.on('click', function (e) {
+            e.preventDefault();
+            
+            var self = $(this);
+            // Use the data-target-element attribute, if it exists. Fall back to href.
+            var target = self.attr('data-target-element') ? self.attr('data-target-element') : self.attr('href');
+            
+            // Get target element
+            var elem = $(target);
+            
+            // Check if stub is open
+            var isSkipContentOpen = elem.hasClass('skip-active') ? 1 : 0;
+            
+            // Hide all stubs
+            skipLinks.removeClass('skip-active');
+            skipContents.removeClass('skip-active');
+            
+            // Toggle stubs
+            if (isSkipContentOpen) {
+                self.removeClass('skip-active');
+            } else {
+                self.addClass('skip-active');
+                elem.addClass('skip-active');
+            }
+        });
 
 
     });
---
+++ templates/molecules/skip-links.php
@@ -0,0 +1,30 @@
+<div class="container">
+    <p>Skip Links are generally a mobile only pattern.</p>
+</div>
+
+
+<div class="container">
+    <div class="container-xs-max">
+        
+        <div class="skip-links">
+            <a href="#header-menu" data-target-element="#header-menu" class="skip-link skip-menu">
+                <span class="label">Menu</span>
+            </a>
+            <a href="#header-search" data-target-element="#header-search" class="skip-link skip-search">
+                <span class="label">Search</span>
+            </a>
+            <a href="#header-account" data-target-element="#header-account" class="skip-link skip-account">
+                <span class="label">Account</span>
+            </a>
+            <a href="#header-cart" class="skip-link skip-cart">
+                <span class="label">Basket</span>
+            </a>
+        </div>
+        
+        <div id="header-menu" class="skip-content skip-menu">Menu goes here</div>
+        <div id="header-search" class="skip-content skip-search">Search goes here</div>
+        <div id="header-account" class="skip-content skip-account">Account links go here</div>
+        <div id="header-cart" class="skip-content skip-cart">Cart (without data attribute on link, falls back to href)</div>
+    
+    </div>  
+</div>
