b1bcdf95b51714393653ec67a1e952eff9e3b480
Gareth <gjoyce@iweb.co.uk>
2015-09-17 10:23:01 +0100

    Commented out Browser sync until we can work out how to use it properly.

--- inc/scripts.php
+++ inc/scripts.php
@@ -4,8 +4,9 @@
 <script src="/build/js/script.min.js"></script>
 <script src="/build/js/primer.min.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>
-
-<script type='text/javascript' id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.8.2.js'><\/script>".replace("HOST", location.hostname));
-//]]></script>
+<script type='text/javascript' id="__bs_script__">
+//<![CDATA[
+document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.8.2.js'><\/script>".replace("HOST", location.hostname));
+//]]>
+</script>
 <!-- End template: _js !-->
