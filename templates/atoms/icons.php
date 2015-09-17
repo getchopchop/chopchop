bfa2b5f937e27a0366c3c645438c0b87581663da
Gareth <gjoyce@iweb.co.uk>
2015-09-17 15:24:18 +0100

    Atoms: replaced icons, merged social and common into one fontello.

--- templates/atoms/icons.php
+++ templates/atoms/icons.php
@@ -1,5 +1,47 @@
 <div class="container">
     
+    
+    <p>All icons are denoted by the prefix <code>icon-</code>. These icons are taken from <a href="http://fontello.com/">fontello.com</a> and used as directed from there.</p>
+    
+    <ul class="list-unstyled">
+        <li>
+            <i class="icon-linkedin"></i>
+            <span class="i-name">icon-linkedin</span>
+        </li>
+        <li>
+            <i class="icon-twitter"></i>
+            <span class="i-name">icon-twitter</span>
+        </li>
+        <li>
+            <i class="icon-facebook"></i>
+            <span class="i-name">icon-facebook</span>
+        </li>
+        <li>
+            <i class="icon-gplus"></i>
+            <span class="i-name">icon-gplus</span>
+        </li>
+        <li>
+            <i class="icon-instagram"></i>
+            <span class="i-name">icon-instagram</span>
+        </li>
+        <li>
+            <i class="icon-youtube"></i>
+            <span class="i-name">icon-youtube</span>
+        </li>
+        <li>
+            <i class="icon-pinterest"></i>
+            <span class="i-name">icon-pinterest</span>
+        </li>
+        <li>
+            <i class="icon-tumblr"></i>
+            <span class="i-name">icon-tumblr</span>
+        </li>
+    </ul>
+    
+    <br/><br/>
+    
+
+    
     <ul class="list-unstyled">
         <li>
             <i class="icon-search"></i>
@@ -25,29 +67,64 @@
             <i class="icon-user"></i>
             <span class="i-name">icon-user</span>
         </li>
+        
+        <li>
+            <i class="icon-info"></i>
+            <span class="i-name">icon-info</span>
+        </li>
+        
+        <li>
+            <i class="icon-help"></i>
+            <span class="i-name">icon-help</span>
+        </li>
+        
+        <li>
+            <i class="icon-heart"></i>
+            <span class="i-name">icon-heart</span>
+        </li>
+        
+        <li>
+            <i class="icon-download-cloud"></i>
+            <span class="i-name">icon-download-cloud</span>
+        </li>
         <li>
             <i class="icon-basket"></i>
             <span class="i-name">icon-basket</span>
         </li>
+        
         <li>
-            <i class="icon-down-dir"></i>
-            <span class="i-name">icon-down-dir</span>
+            <i class="icon-comment"></i>
+            <span class="i-name">icon-comment</span>
         </li>
         <li>
-            <i class="icon-up-dir"></i>
-            <span class="i-name">icon-up-dir</span>
+            <i class="icon-chat"></i>
+            <span class="i-name">icon-chat</span>
+        </li>
+
+        <li>
+            <i class="icon-pencil"></i>
+            <span class="i-name">icon-pencil</span>
         </li>
         <li>
-            <i class="icon-left-dir"></i>
-            <span class="i-name">icon-left-dir</span>
+            <i class="icon-star"></i>
+            <span class="i-name">icon-star</span>
+        </li>
+        
+        <li>
+            <i class="icon-angle-down"></i>
+            <span class="i-name">icon-angle-down</span>
         </li>
         <li>
-            <i class="icon-right-dir"></i>
-            <span class="i-name">icon-right-dir</span>
+            <i class="icon-angle-up"></i>
+            <span class="i-name">icon-angle-up</span>
         </li>
         <li>
-            <i class="icon-down-dir"></i>
-            <span class="i-name">icon-down-dir</span>
+            <i class="icon-angle-left"></i>
+            <span class="i-name">icon-angle-left</span>
+        </li>
+        <li>
+            <i class="icon-angle-right"></i>
+            <span class="i-name">icon-angle-right</span>
         </li>
         <li>
             <i class="icon-menu"></i>
@@ -70,9 +147,25 @@
             <span class="i-name">icon-phone</span>
         </li>
         <li>
-            <i class="icon-mail"></i>
-            <span class="i-name">icon-mail</span>
+            <i class="icon-mail-alt"></i>
+            <span class="i-name">icon-mail-alt</span>
         </li>
+        <li>
+            <i class="icon-lock"></i>
+            <span class="i-name">icon-lock</span>
+        </li>
+
+
+        <li>
+            <i class="icon-attention-alt"></i>
+            <span class="i-name">icon-attention-alt</span>
+        </li>
+
+        <li>
+            <i class="icon-basket"></i>
+            <span class="i-name">icon-basket</span>
+        </li>
+        
         <li>
             <i class="icon-circle-empty"></i>
             <span class="i-name">icon-circle-empty</span>
@@ -82,36 +175,12 @@
             <span class="i-name">icon-dot-circled</span>
         </li>
         <li>
-            <i class="icon-list-bullet"></i>
-            <span class="i-name">icon-list-bullet</span>
-        </li>
-        <li>
-            <i class="icon-mail"></i>
-            <span class="i-name">icon-angle-down</span>
-        </li>
-        <li>
-            <i class="icon-mail"></i>
-            <span class="i-name">icon-angle-up</span>
-        </li>
-        <li>
-            <i class="icon-mail"></i>
-            <span class="i-name">icon-angle-left</span>
-        </li>
-        <li>
-            <i class="icon-mail"></i>
-            <span class="i-name">icon-angle-right</span>
-        </li>
-        <li>
             <i class="icon-th-list"></i>
             <span class="i-name">icon-th-list</span>
         </li>
         <li>
             <i class="icon-th"></i>
             <span class="i-name">icon-th</span>
-        </li>
-        <li>
-            <i class="icon-file-pdf"></i>
-            <span class="i-name">icon-file-pdf</span>
         </li>
     </ul>
     
