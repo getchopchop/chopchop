b6a69a6857c7fe8a1352c43f7708e9247c0e2d87
Gareth <gjoyce@iweb.co.uk>
2015-09-18 14:48:25 +0100

    Chop Nav: added collapsing js and styling hook/hide/show styles and plus minus styles.

--- inc/nav.php
+++ inc/nav.php
@@ -1,17 +1,26 @@
 <?php
-foreach(array('overview', 'atoms', 'molecules', 'organisms', 'grid', 'mixins', 'utilities', 'credits') as $toplevel) {
+foreach(array('overview', 'atoms', 'molecules', 'organisms', 'grid', 'mixins', 'utilities') as $toplevel) {
 ?>
-<nav class="nav--stack nav-<?= $toplevel ?>">
+<nav class="nav--stack nav-<?= $toplevel ?> nav-collapse">
     <ul>
-        <li><a href="/<?= $toplevel ?>"><?= ucwords($toplevel) ?></a></li>
-        <?php
-        $quarks = dir('templates/' . $toplevel);
-        while (false !== ($entry = $quarks->read())) {
-            if(substr($entry, -4) == '.php') {
-                $entry = substr($entry, 0, strlen($entry) - 4);
-        ?>
-        <li><a href="/<?= $toplevel ?>/<?= $entry ?>"><?= str_replace('-', ' ', ucwords($entry)) ?></a></li>
-        <?php }} ?>
+        <li class="level0 parent"><a href="/<?= $toplevel ?>"><?= ucwords($toplevel) ?><i class="icon-plus"></i> <i class="icon-minus"></i></a>
+            <ul class="child">
+                <?php
+                $quarks = dir('templates/' . $toplevel);
+                while (false !== ($entry = $quarks->read())) {
+                    if(substr($entry, -4) == '.php') {
+                        $entry = substr($entry, 0, strlen($entry) - 4);
+                ?>
+                <li class="level1"><a href="/<?= $toplevel ?>/<?= $entry ?>"><?= str_replace('-', ' ', ucwords($entry)) ?></a></li>
+                <?php }} ?>
+            </ul>
+        </li>
     </ul>
 </nav>
 <?php } ?>
+
+<nav class="nav--stack nav-credits">
+    <ul>
+        <li class="level0 parent"><a href="/credits">Credits</a></li>
+    </ul>
+</nav>
--- inc/scripts.php
+++ inc/scripts.php
@@ -2,11 +2,14 @@
 <script src="/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="/bower_components/highlightjs/highlight.pack.min.js"></script>
 <script src="/build/js/script.min.js"></script>
-<script src="/build/js/primer.min.js"></script>
+<script src="/build/js/chopchop.min.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>
+
+<?php /* ?>
 <script type='text/javascript' id="__bs_script__">
 //<![CDATA[
 document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.8.2.js'><\/script>".replace("HOST", location.hostname));
 //]]>
 </script>
+<?php */ ?>
 <!-- End template: _js !-->
