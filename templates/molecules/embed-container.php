3d258d93acd13be8f62d14adb8b57c189ee18158
Gareth <gjoyce@iweb.co.uk>
2015-09-18 15:47:57 +0100

    Molecules: added embed container.

---
+++ templates/molecules/embed-container.php
@@ -0,0 +1,33 @@
+<div class="well">
+    <div class="container">
+        <h2>16x9 Embed container (default)</h2>
+        <p>The default aspect ration of an embed container is 16:9.</p>
+        <div class="embed-container">
+            <iframe src="//www.youtube.com/embed/qrO4YZeyl0I" frameborder="0" allowfullscreen=""></iframe>
+        </div>
+        
+        {% highlight code %}
+<div class="embed-container">
+    <iframe src="//www.youtube.com/embed/qrO4YZeyl0I" frameborder="0" allowfullscreen=""></iframe>
+</div>
+        {% end highlight code %}
+        
+    </div>
+</div>
+
+<div class="well">
+    <div class="container">
+        <h2>4x3 modifier</h2>
+        <p>Using the modifier <code>embed-container--4x3</code> changes the aspect ratio to 4:3.</p>
+        <div class="embed-container embed-container--4x3">
+            <iframe src="//www.youtube.com/embed/qrO4YZeyl0I" frameborder="0" allowfullscreen=""></iframe>
+        </div>
+        
+        {% highlight code %}
+<div class="embed-container embed-container--4x3">
+    <iframe src="//www.youtube.com/embed/qrO4YZeyl0I" frameborder="0" allowfullscreen=""></iframe>
+</div>
+        {% end highlight code %}
+        
+    </div>
+</div>
