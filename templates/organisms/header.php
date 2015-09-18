3925b4aeaf0a62a3ce06851ca7af4ddc9ae8dd71
Gareth <gjoyce@iweb.co.uk>
2015-09-18 16:55:46 +0100

    Organismas: adding content for header organism.

--- templates/organisms/header.php
+++ templates/organisms/header.php
@@ -1,3 +1,30 @@
-<header>
-    feed me
-</header>
+<div class="well">
+    <div class="container">
+        
+        <header>
+            
+            <ul class="u-list-inline">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            
+            <a href="/" class="logo">
+                <span class="logo__art"><img src="/src/img/chopper_black.svg" class="logo__img" /></span>
+                <span class="logo__text"><span>Chop</span> <span>Chop</span></span>
+            </a>
+            
+            <form action="#" method="post" class="form-inline form-search">
+                <label class="is-vishidden">Search the site</label>     
+                <input type="search" placeholder="Search the site" class="form-inline__query" />
+                <input type="submit" class="btn search-submit" value="Search">
+            </form>
+            
+            <a href="tel:01234 567 890">01234 567 890</a>
+            <?php include('templates/molecules/navs/multi-level.php'); ?>
+            
+        </header>
+
+
+    </div>
+</div>
