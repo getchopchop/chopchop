fb2454171f104fe827f34b0067cb98717c84ffcb
Dave Redfern <dave@daveredfern.com>
2015-10-08 14:44:13 +0100

    Making inline forms, flexed

---
+++ templates/molecules/form-inline.php
@@ -0,0 +1,9 @@
+<div class="u-container">
+
+    <form action="#" method="post" class="form-inline">
+        <label class="u-visually-hidden">Search the site</label>
+        <input type="search" placeholder="Enter something into this input">
+        <input type="submit" class="btn" value="Submit">
+    </form>
+
+</div>
