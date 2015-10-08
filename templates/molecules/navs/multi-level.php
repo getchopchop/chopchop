c2f0f54f9d2cfe794d5e4f9978a7e1e435931f25
Gareth <gjoyce@iweb.co.uk>
2015-10-08 15:54:30 +0100

    Navs: all are good and solid.

--- templates/molecules/navs/multi-level.php
+++ templates/molecules/navs/multi-level.php
@@ -1,81 +1,52 @@
-<nav class="nav--primary nav--multi-level">
-
-    <ol>
-        <li class="level0 nav-1 first parent">
-            <a href="#" class="level0 has-children">First Level 0</a>
-            <ul class="level0">
-                <li class="level1">
-                    <a class="level1" href="lighting.html">Level 1</a>
-                </li>
-                <li class="level1 nav-1-1 first parent">
-                    <a href="#" class="level1 has-children">Level 1</a>
-                    <ul class="level1">
-                        <li class="level2">
-                            <a class="level2" href="#">Level 2</a>
-                        </li>
-                        <li class="level2 nav-1-1-1 first parent">
-                            <a href="#" class="level2 has-children">Level 2</a>
-                            <ul class="level2">
-                                <li class="level3">
-                                    <a class="level3" href="#">Level 3</a>
-                                </li>
-                                <li class="level3 nav-1-1-1-1 first last parent">
-                                    <a href="#" class="level3 has-children">Level 3</a>
-                                    <ul class="level3">
-                                        <li class="level4">
-                                            <a class="level4" href="#">Level 4</a>
-                                        </li>
-                                        <li class="level4 nav-1-1-1-1-1 first last">
-                                            <a href="#" class="level4 ">Level 4</a>
-                                        </li>
+<nav>
+    <ol class="nav nav--pills nav--inline nav--multi-level">
+        <li>
+            <a href="#">First Level 0</a>
+            <ul>
+                <li><a href="#">Level 1</a></li>
+                <li>
+                    <a href="#">Level 1</a>
+                    <ul>
+                        <li><a href="#">Level 2</a></li>
+                        <li>
+                            <a href="#">Level 2</a>
+                            <ul>
+                                <li><a href="#">Level 3</a></li>
+                                <li>
+                                    <a href="#">Level 3</a>
+                                    <ul>
+                                        <li><a href="#">Level 4</a></li>
+                                        <li><a href="#">Level 4</a></li>
                                     </ul>
                                 </li>
                             </ul>
                         </li>
-                        <li class="level2 nav-1-1-2 parent">
-                            <a href="#" class="level2 has-children">Level 2</a>
-                            <ul class="level2">
-                                <li class="level3">
-                                    <a class="level3" href="#">Level 3</a>
-                                </li>
-                                <li class="level3 nav-1-1-2-1 first last">
-                                    <a href="#" class="level3 ">Level 3</a>
-                                </li>
+                        <li>
+                            <a href="#">Level 2</a>
+                            <ul>
+                                <li><a href="#">Level 3</a></li>
+                                <li><a href="#" class="level3 ">Level 3</a></li>
                             </ul>
                         </li>
-                        <li class="level2 nav-1-1-3 parent">
-                            <a href="#" class="level2 has-children">Level 2</a>
-                            <ul class="level2">
-                                <li class="level3">
-                                    <a class="level3" href="#">Level 3</a>
-                                </li>
-                                <li class="level3 nav-1-1-3-1 first last">
-                                    <a href="#" class="level3 ">Level 3</a>
-                                </li>
+                        <li>
+                            <a href="#">Level 2</a>
+                            <ul>
+                                <li><a href="#">Level 3</a> </li>
+                                <li><a href="#" class="level3 ">Level 3</a></li>
                             </ul>
                         </li>
-                        <li class="level2 nav-1-1-4 last parent">
-                            <a href="#" class="level2 has-children">Level 2</a>
-                            <ul class="level2">
-                                <li class="level3">
-                                    <a class="level3" href="#">Level 3</a>
-                                </li>
-                                <li class="level3 nav-1-1-4-1 first last">
-                                    <a href="#" class="level3 ">Level 3</a>
-                                </li>
+                        <li>
+                            <a href="#">Level 2</a>
+                            <ul>
+                                <li><a href="#">Level 3</a></li>
+                                <li><a href="#">Level 3</a></li>
                             </ul>
                         </li>
                     </ul>
                 </li>
             </ul>
         </li>
-        
-        <li class="level0 nav-2">
-            <a href="#" class="level0">Level 0</a>
-        </li>
-        <li class="level0 nav-3 last">
-            <a href="#" class="level0">Last Level 0</a>
-        </li>
+        <li><a href="#">Level 0</a></li>
+        <li><a href="#">Last Level 0</a></li>
     </ol>
-    
 </nav>
