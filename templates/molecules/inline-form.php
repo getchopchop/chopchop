013630bda9ff1ed2a278d4261b8203b06a92859f
Dave Redfern <dave@daveredfern.com>
2015-10-08 14:49:15 +0100

    Renaming form-inline to inline-form to match other naming conventions

--- templates/molecules/inline-form.php
+++ templates/molecules/inline-form.php
@@ -1,6 +1,6 @@
 <div class="u-container">
 
-    <form action="#" method="post" class="form-inline">
+    <form action="#" method="post" class="inline-form">
         <label class="u-visually-hidden">Search the site</label>
         <input type="search" placeholder="Enter something into this input">
         <input type="submit" class="btn" value="Submit">
