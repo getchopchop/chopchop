147c4833e427b8b92cd186d4efd8a619c0298ff4
Dave Redfern <dave@daveredfern.com>
2015-10-15 15:43:31 +0100

    Improvements to cards

--- templates/molecules/cards.php
+++ templates/molecules/cards.php
@@ -1,105 +1,81 @@
 
 <div class="u-container">
 
-    <h3>Simple Card</h3>
-    <div class="card">
-        <div class="card__header">
-            Card Header
-        </div>
-        <div class="card__body">
-            Card Body
-        </div>
-        <div class="card__footer">
-            Card Footer
-        </div>
-    </div>
-	
+    <div class="g-three-up g-gutter">
 
-    <h3>Card with Image Cap</h3>
-    <div class="card">
-        <img class="card__cap" src="//placehold.it/400x240" alt="Card image cap">
-        <div class="card__body">
-            <h4 class="card__title">Card Title</h4>
-            <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn--primary">Button</a>
-        </div>
-    </div>
-
-    <h3>Card with Title</h3>
-
-    <div class="card">
-        <div class="card__body">
-            <h4 class="card__title">Card Title</h4>
-            <h6 class="card__subtitle">Supporting title</h6>
-        </div>
-        <img class="card__img" src="//placehold.it/400x240" alt="Card image">
-        <div class="card__body">
-            <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn--primary">Button</a>
-        </div>
-    </div>
-
-    <h2>Cards in Columns</h2>
-
-    <div class="card-columns">
-        <div class="card">
-            <img class="card__cap" src="//placehold.it/400x240" alt="Card image cap">
-            <div class="card__body">
-                <h4 class="card__title">Card Title</h4>
-                <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn--primary">Button</a>
+        <div>
+            <h3>Simple Card</h3>
+            <div class="card">
+                <div class="card__header">
+                    Card Header
+                </div>
+                <div class="card__body">
+                    Card Body
+                </div>
+                <div class="card__footer">
+                    Card Footer
+                </div>
             </div>
         </div>
 
-        <div class="card">
-            <div class="card__body">
-                <h4 class="card__title">Card Title</h4>
-                <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn--primary">Button</a>
+        <div>
+            <h3>Card with Image at top</h3>
+            <div class="card">
+                <img class="card__img" src="//placehold.it/400x240" alt="Card image">
+                <div class="card__body">
+                    <h4>Card Title</h4>
+                    <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p><a href="#" class="btn btn--primary">Button</a></p>
+                </div>
             </div>
         </div>
 
-        <div class="card">
-            <div class="card__body">
-                <h4 class="card__title">Card Title</h4>
-                <h6 class="card__subtitle">Supporting title</h6>
-            </div>
-            <img class="card__img" src="//placehold.it/400x240" alt="Card image cap">
-            <div class="card__body">
-                <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn--primary">Button</a>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="card__body">
-                <h4 class="card__title">Card Title</h4>
-                <h6 class="card__subtitle">Supporting title</h6>
-            </div>
-            <img class="card__img" src="//placehold.it/400x240" alt="Card image cap">
-            <div class="card__body">
-                <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn--primary">Button</a>
+        <div>
+            <h3>Card with Title</h3>
+            <div class="card">
+                <div class="card__header">
+                    <h4 class="card__title">Card Title</h4>
+                    <h6 class="card__subtitle">This is the sub title</h6>
+                </div>
+                <img class="card__img" src="//placehold.it/400x240" alt="Card image">
+                <div class="card__body">
+                    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p><a href="#" class="btn btn--primary">Button</a></p>
+                </div>
             </div>
         </div>
 
-        <div class="card">
-            <div class="card__body">
-                <h4 class="card__title">Card Title</h4>
-                <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn--primary">Button</a>
+        <div>
+            <h3>Card with Title</h3>
+            <div class="card">
+                <div class="card__header">
+                    <h4 class="card__title">Card Title</h4>
+                    <h6 class="card__subtitle">This is the sub title</h6>
+                </div>
+                <img class="card__img" src="//placehold.it/400x240" alt="Card image">
+                <div class="card__body">
+                    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="card__footer">
+                    <p><a href="#" class="btn btn--primary">Button</a></p>
+                </div>
             </div>
         </div>
 
-        <div class="card">
-            <div class="card__body">
-                <h4 class="card__title">Card Title</h4>
-                <h6 class="card__subtitle">Supporting title</h6>
-            </div>
-            <img class="card__img" src="//placehold.it/400x240" alt="Card image cap">
-            <div class="card__body">
-                <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn--primary">Button</a>
+        <div>
+            <h3>Card with border</h3>
+            <div class="card card--bordered">
+                <div class="card__header">
+                    <h4 class="card__title">Card Title</h4>
+                    <h6 class="card__subtitle">This is the sub title</h6>
+                </div>
+                <img class="card__img" src="//placehold.it/400x240" alt="Card image">
+                <div class="card__body">
+                    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="card__footer">
+                    <p><a href="#" class="btn btn--primary">Button</a></p>
+                </div>
             </div>
         </div>
 
