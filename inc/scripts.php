3a0ad7dc3cf85667313fb8c3fad28ebdec450e4b
Dave Redfern <dave@daveredfern.com>
2015-10-22 15:38:31 +0100

    Adding picturefill and sorting scripts

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,15 +1,6 @@
 <!-- Template: _js !-->
 <script src="/bower_components/jquery/dist/jquery.min.js"></script>
-<script src="/bower_components/highlightjs/highlight.pack.min.js"></script>
+<script src="/bower_components/picturefill/dist/picturefill.min.js"></script>
 <script src="/build/js/script.js"></script>
 <script src="/build/js/chopchop.js"></script>
-<script>hljs.initHighlightingOnLoad();</script>
-
-<?php /* ?>
-<script type='text/javascript' id="__bs_script__">
-//<![CDATA[
-document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.8.2.js'><\/script>".replace("HOST", location.hostname));
-//]]>
-</script>
-<?php */ ?>
 <!-- End template: _js !-->
--- inc/styles.php
+++ inc/styles.php
@@ -1,9 +1,7 @@
 <!-- Template: _styles !-->
 <link rel="stylesheet" type="text/css" href="/bower_components/normalize.css/normalize.css" media="all" />
-<link rel="stylesheet" type="text/css" href="/bower_components/highlightjs/styles/github.css" media="all" />
+<link rel="stylesheet" type="text/css" href="/src/vendor/fontello/css/fontello.css" media="all" />
+
 <link rel="stylesheet" type="text/css" href="/build/css/styles.css" media="all" />
 <link rel="stylesheet" type="text/css" href="/build/css/chopchop-ui.css" media="all" />
-
-<link rel="stylesheet" type="text/css" href="/src/vendor/fontello/css/fontello.css" media="all" />
-<link rel="stylesheet" type="text/css" href="/src/vendor/webfontkit-20150925-063227/stylesheet.css" media="all" />
 <!-- End template: _styles !-->
