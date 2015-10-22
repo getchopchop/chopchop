dd45fa40ffadd142530104412b6c5cd87f9feabf
Bas Mostert <bas@iweb.co.uk>
2015-10-22 22:13:08 +0100

    Completely rework toggle javascript

--- src/js/chopchop.lib.js
+++ src/js/chopchop.lib.js
@@ -3,63 +3,111 @@ var ChopChop = (function($, ChopChop) {
     var defaultOptions = {
             classActive: 'is-active',
             classInactive: 'is-inactive'
-        },
-        processed = {};
+        };
 
+    // Exception
+    var Exception = ChopChop.Exception = function(message) {
+        this.message = message;
+    };
+
+    Exception.prototype = {
+        toString: function() {
+            return 'EXCEPTION: ' + this.message;
+        }
+    };
+
+    // Toggle
     var Toggle = ChopChop.Toggle = function(root, options) {
         this.options = $.extend({}, defaultOptions, options);
         this.$root = $(root);
         this.init();
     };
+	
+	var Mode = {
+		TOGGLE: 'toggle',
+		ACTIVATE: 'activate',
+		DEACTIVATE: 'deactivate'
+	};
 
     Toggle.prototype = {
         init: function() {
             var self = this;
 
-            $(document).on('click', '[data-toggle-target]', function(e) {
+            $(document).on('click', '[data-toggle-action]', function(e) {
                 e.preventDefault();
-                self.toggleTarget($(this).data('toggle-target'));
+                var $this = $(this), t,
+                    $target = (t = $this.data('toggle-target')) ? $('[data-toggle-id="' + t + '"]') : $this;
+
+				self.toggle($target, $this.data('toggle-action') || Mode.TOGGLE);
             });
         },
-        toggleTarget: function(target) {
-            var $target, group, groupMembers, isActive, $this,
-                cascades = [], cascade,
-                self = this;
+		toggle: function($el, action) {
+			var mode = action;
 
-            target.split(' ').forEach(function(target) {
-                // Prevent infinite recusion possibly via cascades
-                if (processed[target]) {
-                    return;
+			if (mode === Mode.TOGGLE) {
+				if ($el.hasClass('is-active')) {
+					mode = Mode.DEACTIVATE;
+				} else {
+					mode = Mode.ACTIVATE;
+				}
+			}
+			
+			var pending = [$el], processed = [],
+				id, dependants, group, $current,
+                iterations = 0;
+			
+			while (pending.length > 0) {
+				$current = pending.shift();
+				
+				// Skip already processed nodes
+				if (processed.indexOf($current) !== -1) {
+					continue;
+				}
+				
+				id = $current.data('toggle-id');
+				group = $current.data('toggle-group');
+				
+				// NOTE: Perhaps don't process these; add to some kind of queue first (with activate or deactivate action)
+				$('[data-toggle-group="' + group + '"]').each(function() {
+					var $this = $(this), chain;
+					
+                    // Skip element itself
+					if (this === $current[0]) {
+                        return;
+                    }
+
+                    // Just a group member, not the target: deactivate
+                    $this.addClass('is-inactive').removeClass('is-active');														
+				});
+				
+                // Process target
+                if (mode === Mode.ACTIVATE) {
+                    $current.addClass('is-active').removeClass('is-inactive');
+                } else {
+                    $current.addClass('is-inactive').removeClass('is-active');
+                }
+                
+                // Chain onto targets
+                if (chain = $current.data('toggle-' + mode)) {
+                    chain.split(',').forEach(function(chained) {
+                        var $chained = $('[data-toggle-id="' + chain + '"]');
+
+                        if (processed.indexOf($chained[0]) !== -1 || pending.indexOf($chained[0]) !== -1) {
+                            return;
+                        }
+
+                        pending.push($('[data-toggle-id="' + chained + '"]'));
+                    });
                 }
 
-                processed[target] = true;
+				processed.push($current[0]);
+                iterations += 1;
 
-                $target = self.$root.find('[data-toggle="' + target + '"]');
-                group = $target.data('toggle-group');
-
-                $('[data-toggle-group="' + group + '"]').each(function() {
-                    $this = $(this);
-
-                    if (this === $target[0]) {
-                        $this.removeClass('is-inactive').addClass('is-active');
-
-                        var cascade = $this.data('toggle-cascade');
-                        if (cascade) {
-                            cascades.push(cascade);
-                        }
-                    } else {
-                        $this.removeClass('is-active').addClass('is-inactive');
-                    }
-                });
-
-                // Process any collected cascades
-                cascades.forEach(function(cascade) {
-                    self.toggleTarget(cascade);
-                });
-
-                delete processed[target];
-            });
-        }
+                if (iterations > 100) {
+                    throw new ChopChop.Exception("Too many iterations, aborting");
+                }
+			}
+		}
     };
 
     return ChopChop;
