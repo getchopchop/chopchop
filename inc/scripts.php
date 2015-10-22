cf55749e994db4c6f3d28a422a1d046d01a7bd06
Dave Redfern <dave@daveredfern.com>
2015-10-22 14:59:39 +0100

    Fixing paths to assets

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,8 +1,8 @@
 <!-- Template: _js !-->
 <script src="/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="/bower_components/highlightjs/highlight.pack.min.js"></script>
-<script src="/build/js/script.min.js"></script>
-<script src="/build/js/chopchop.min.js"></script>
+<script src="/build/js/script.js"></script>
+<script src="/build/js/chopchop.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>
 
 <?php /* ?>
--- inc/styles.php
+++ inc/styles.php
@@ -1,8 +1,8 @@
 <!-- Template: _styles !-->
 <link rel="stylesheet" type="text/css" href="/bower_components/normalize.css/normalize.css" media="all" />
 <link rel="stylesheet" type="text/css" href="/bower_components/highlightjs/styles/github.css" media="all" />
-<link rel="stylesheet" type="text/css" href="/build/css/styles.min.css" media="all" />
-<link rel="stylesheet" type="text/css" href="/build/css/chopchop-ui.min.css" media="all" />
+<link rel="stylesheet" type="text/css" href="/build/css/styles.css" media="all" />
+<link rel="stylesheet" type="text/css" href="/build/css/chopchop-ui.css" media="all" />
 
 <link rel="stylesheet" type="text/css" href="/src/vendor/fontello/css/fontello.css" media="all" />
 <link rel="stylesheet" type="text/css" href="/src/vendor/webfontkit-20150925-063227/stylesheet.css" media="all" />
