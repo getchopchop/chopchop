9253e255e1246dba7e89d583d0704aaecfaf563c
Dave Redfern <dave@daveredfern.com>
2015-11-10 12:47:29 +0000

    Basic styling for tabs

--- toggles.php
+++ toggles.php
@@ -18,7 +18,7 @@
             <div class="accordion__header">
                 <h3>Tab 1</h3>
             </div>
-            <div class="accordion__body">
+            <div class="is-active accordion__body">
                 <p>tab 1 content</p>
             </div>
             <div class="accordion__header">
@@ -81,10 +81,10 @@
         <h2>Accordion that can have multiple open at once</h2>
 
         <div class="accordion">
-            <div class="accordion__header">
+            <div class="is-active accordion__header">
                 <h3>Tab 1</h3>
             </div>
-            <div class="accordion__body">
+            <div class="is-active accordion__body">
                 <p>tab 1 content</p>
             </div>
             <div class="accordion__header">
@@ -93,10 +93,10 @@
             <div class="accordion__body">
                 <p>tab 2 content</p>
             </div>
-            <div class="accordion__header">
+            <div class="is-active accordion__header">
                 <h3>Tab 3</h3>
             </div>
-            <div class="accordion__body">
+            <div class="is-active accordion__body">
                 <p>tab 3 content</p>
             </div>
             <div class="accordion__header">
@@ -114,10 +114,10 @@
         <h2>Tabs</h2>
 
         <div class="tabs">
-            <div class="tabs__header">
+            <div class="is-active tabs__header">
                 <h3>Tab 1</h3>
             </div>
-            <div class="tabs__body">
+            <div class="is-active tabs__body">
                 <p>tab 1 content</p>
             </div>
             <div class="tabs__header">
