d154d04b80add50a46475cac7715f14054858cc9
Gareth <gjoyce@iweb.co.uk>
2015-11-12 16:01:11 +0000

    Molecules: Toggle demos split into molecules.

--- static/molecules/drawers.php
+++ static/molecules/drawers.php
@@ -1,52 +1,56 @@
-<div class="well">
-    <div class="u-container">
-        <h2>Drawers</h2>
-        <p>Drawers are special molecules that need thier own page to demonstrate how they work.</p>
-        <p><a href="/drawers.php" class="btn" target="_blank">View Demo</a></p>
-        <p>Drawers are particularly useful for mobile devices where the screen sizes dont allow for a lot of content to be displayed on the screen at one time.</p>
-        
-    </div>
-</div>
-<div class="well">
-    <div class="u-container">
-        
-        <h3>Required Data Attributes</h3>
-        <p class="lead">Drawers are hidden by default and need a toggle in order to show them on screen.</p>
-        <p>A drawer toggle needs a <code>data-toggle="drawer"</code> attribute and a <code>data-target</code> attribute, the value of which must match the id of the drawer.</p>
+<div class="u-container">
+    
+    <p class="cc-heading">Drawers <code>.drawer</code></p>
+    
+    <p><a href="#" data-toggle-action="activate" data-toggle-activate="drawer-left">Left drawer</a></p>
+    <p><a href="#" data-toggle-action="activate" data-toggle-activate="drawer-right">Right drawer</a></p>
+    
+    <div class="overlay" 
+        data-toggle-id="overlay" 
+        data-toggle-action="deactivate"
+        data-toggle-deactivate="drawer-left,drawer-right,modal">
+    </div> <!-- toggle is-active to show !-->
 
-    </div>
-</div>
-<div class="well">
-    <div class="u-container">
-        
-        <h3>Optional Data Attributes.</h3>
-        <p>There are also some data attributes that aren't required to make the drawers function. Adding a <code>data-content</code> attribute will show blocks within the given drawer providing the value matches a given class.</p>
-        <p>Adding a <code>data-title</code> attribute will make any other element with the class of title--drawer change to the given value, <a href="/drawers.php" target="_blank">see demo</a>.</p>
+    <!-- toggle is-active to show !-->
+    <div class="drawer drawer--left" 
+        data-toggle-id="drawer-left"
+        data-toggle-group="drawers"
+        data-toggle-activate="overlay">
 
-{% highlight code %}
-<a href="#" class="btn" data-toggle="drawer" data-target="left" data-content="lc1" data-title="Look the title changes: left content block 1">Left Content Block 1</a>
-{% end highlight code %}
+        <div class="drawer__dialog card">
+            <div class="card__header">
+                <p class="cc-heading">Left Drawer <code>.drawer--left</code></p>
+            </div>
+            <div class="card__body">
+                <p>Vestibulum varius sem vitae diam venenatis porttitor. Pellentesque nec quam eget eros tincidunt mattis non ac nunc. Vivamus tristique, turpis vitae efficitur laoreet, eros tellus lobortis nulla, ac auctor neque velit at risus. Sed volutpat dolor erat, placerat malesuada quam tempor eu. Morbi a tellus sit amet lectus tincidunt imperdiet a tincidunt est. Cras vel volutpat mauris. Sed consectetur felis vel maximus elementum. Proin sed nisl lectus. Nunc non est ac ipsum gravida commodo vel quis nunc.</p>
+                <p>In convallis sem quis risus suscipit efficitur. Vivamus neque nisl, aliquam ac auctor nec, dignissim ac elit. Vivamus et enim augue. Cras mauris ex, pretium eget erat in, hendrerit tempus elit. Nam nisl tortor, vestibulum at arcu sit amet, posuere tincidunt enim. Nunc luctus sagittis sapien, quis tempor ligula mattis scelerisque. Quisque condimentum justo massa, vel egestas enim tempus et. Cras iaculis ex a velit molestie tempor. Aliquam aliquet faucibus rutrum. Aenean pharetra mattis imperdiet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque cursus risus non magna lobortis fermentum. Nulla accumsan nulla ac elit blandit, sed porta quam condimentum.</p>
+                <p>Ut malesuada felis pulvinar nulla tempor tempus. Donec at quam scelerisque, egestas augue dignissim, gravida mi. Cras at pellentesque mi, quis tristique felis. In auctor vel mauris at tempus. Fusce fermentum vel mi sit amet faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer pretium, mauris et imperdiet pulvinar, velit elit rhoncus lacus, nec volutpat turpis elit non arcu. Curabitur vel lorem ac lacus consectetur consequat. Ut nulla justo, viverra nec urna et, pharetra varius orci. Aenean sit amet lorem nunc. Vivamus sagittis accumsan justo, id luctus leo facilisis eget. Aliquam bibendum erat commodo, consectetur erat non, varius neque.</p>
+            </div>
+            <div class="card__footer">
+                <p>Some footer bits</p>
+            </div>
+        </div>
+    </div>
 
+    <!-- toggle class is-active to show !-->
+    <div class="drawer drawer--right"
+        data-toggle-id="drawer-right"
+        data-toggle-group="drawers"
+        data-toggle-activate="overlay">
+
+        <div class="drawer__dialog card">
+            <div class="card__header">
+                <p class="cc-heading">Right Drawer <code>.drawer--right</code></p>
+            </div>
+            <div class="card__body">
+                <p>Vestibulum varius sem vitae diam venenatis porttitor. Pellentesque nec quam eget eros tincidunt mattis non ac nunc. Vivamus tristique, turpis vitae efficitur laoreet, eros tellus lobortis nulla, ac auctor neque velit at risus. Sed volutpat dolor erat, placerat malesuada quam tempor eu. Morbi a tellus sit amet lectus tincidunt imperdiet a tincidunt est. Cras vel volutpat mauris. Sed consectetur felis vel maximus elementum. Proin sed nisl lectus. Nunc non est ac ipsum gravida commodo vel quis nunc.</p>
+                <p>In convallis sem quis risus suscipit efficitur. Vivamus neque nisl, aliquam ac auctor nec, dignissim ac elit. Vivamus et enim augue. Cras mauris ex, pretium eget erat in, hendrerit tempus elit. Nam nisl tortor, vestibulum at arcu sit amet, posuere tincidunt enim. Nunc luctus sagittis sapien, quis tempor ligula mattis scelerisque. Quisque condimentum justo massa, vel egestas enim tempus et. Cras iaculis ex a velit molestie tempor. Aliquam aliquet faucibus rutrum. Aenean pharetra mattis imperdiet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque cursus risus non magna lobortis fermentum. Nulla accumsan nulla ac elit blandit, sed porta quam condimentum.</p>
+                <p>Ut malesuada felis pulvinar nulla tempor tempus. Donec at quam scelerisque, egestas augue dignissim, gravida mi. Cras at pellentesque mi, quis tristique felis. In auctor vel mauris at tempus. Fusce fermentum vel mi sit amet faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer pretium, mauris et imperdiet pulvinar, velit elit rhoncus lacus, nec volutpat turpis elit non arcu. Curabitur vel lorem ac lacus consectetur consequat. Ut nulla justo, viverra nec urna et, pharetra varius orci. Aenean sit amet lorem nunc. Vivamus sagittis accumsan justo, id luctus leo facilisis eget. Aliquam bibendum erat commodo, consectetur erat non, varius neque.</p>
+            </div>
+            <div class="card__footer">
+                <p>Some footer bits</p>
+            </div>
+        </div>
     </div>
-</div>
-<div class="well">
-    <div class="u-container">
-        
-        <h2>Drawer Code Example</h2>
-        <p>Simple left drawer example.</p>
-        
-{% highlight code %}
-<aside role="drawer" id="drawer-left" class="drawer drawer--left">
-  <div class="drawer__dialog">
-    <div class="drawer__body">
-      <div class="drawer__content">
-        <div class="lc1 toggleable">Left Content 1</div>
-        <div class="lc2 toggleable">Left Content 2</div>
-      </div><!--/drawer__content-->
-    </div><!--/drawer__body-->
-  </div>
-</aside><!--/drawer--left-->
-{% end highlight code %}
-        
-    </div>
+    
 </div>
--- static/molecules/modals.php
+++ static/molecules/modals.php
@@ -1,31 +1,37 @@
-<div id="myModal" class="modal modal--lg is-active">
-    
-    <a href="#" class="btn btn--sm btn--blue btn--square modal__close">
-        <span class="btn__text">&times;</span>
-    </a>
-    
-    <div class="modal__dialog">
-
-        <div class="modal__header">
-            <h3 class="modal__title">This is a modal, plain and simple.</h3>
-        </div>
-        
-        <div class="modal__body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. </p>
-
-            <p>Sed dignissim lacinia nunc. <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit</i>. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. <b>Curabitur tortor</b>. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit</i>. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. </p>
-
-            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <b>Maecenas mattis</b>. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. </p>
-        </div>
-        
-        <div class="modal__footer">
-            <a href="#" class="btn btn--sm btn--blue">
-                <span class="icon-cancel"></i>
-                <span class="btn__text">Close Me</span>
-            </a>
-        </div>
-        
-    </div>
+<div class="u-container">
+    <p>
+        <a href="#" 
+            data-toggle-action="activate"
+            data-toggle-activate="modal">
+                Click me for modal time
+        </a>
+    </p>
 </div>
 
-<div class="overlay is-active"></div>
+<div class="overlay" 
+    data-toggle-id="overlay" 
+    data-toggle-action="deactivate"
+    data-toggle-deactivate="drawer-left,drawer-right,modal">
+</div> <!-- toggle is-active to show !-->
+
+
+<!-- toggle is-active to show !-->
+<div class="modal"
+    data-toggle-id="modal"
+    data-toggle-activate="overlay">
+
+    <div class="modal__dialog card">
+        <div class="card__header">
+            <h2>This is a modal</h2>
+        </div>
+        <div class="card__body">
+            <p>Vestibulum varius sem vitae diam venenatis porttitor. Pellentesque nec quam eget eros tincidunt mattis non ac nunc. Vivamus tristique, turpis vitae efficitur laoreet, eros tellus lobortis nulla, ac auctor neque velit at risus. Sed volutpat dolor erat, placerat malesuada quam tempor eu. Morbi a tellus sit amet lectus tincidunt imperdiet a tincidunt est. Cras vel volutpat mauris. Sed consectetur felis vel maximus elementum. Proin sed nisl lectus. Nunc non est ac ipsum gravida commodo vel quis nunc.</p>
+            <p>In convallis sem quis risus suscipit efficitur. Vivamus neque nisl, aliquam ac auctor nec, dignissim ac elit. Vivamus et enim augue. Cras mauris ex, pretium eget erat in, hendrerit tempus elit. Nam nisl tortor, vestibulum at arcu sit amet, posuere tincidunt enim. Nunc luctus sagittis sapien, quis tempor ligula mattis scelerisque. Quisque condimentum justo massa, vel egestas enim tempus et. Cras iaculis ex a velit molestie tempor. Aliquam aliquet faucibus rutrum. Aenean pharetra mattis imperdiet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque cursus risus non magna lobortis fermentum. Nulla accumsan nulla ac elit blandit, sed porta quam condimentum.</p>
+            <p>Ut malesuada felis pulvinar nulla tempor tempus. Donec at quam scelerisque, egestas augue dignissim, gravida mi. Cras at pellentesque mi, quis tristique felis. In auctor vel mauris at tempus. Fusce fermentum vel mi sit amet faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer pretium, mauris et imperdiet pulvinar, velit elit rhoncus lacus, nec volutpat turpis elit non arcu. Curabitur vel lorem ac lacus consectetur consequat. Ut nulla justo, viverra nec urna et, pharetra varius orci. Aenean sit amet lorem nunc. Vivamus sagittis accumsan justo, id luctus leo facilisis eget. Aliquam bibendum erat commodo, consectetur erat non, varius neque.</p>
+        </div>
+        <div class="card__footer">
+            <p>Check my footer out</p>
+        </div>
+    </div>
+    <a href="#" class="btn modal__close" data-toggle-action="deactivate" data-toggle-deactivate="overlay">Close</a>
+</div>
