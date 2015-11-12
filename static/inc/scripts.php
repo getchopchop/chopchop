164f7e33b431ea606ac77382a4a114b8bdd3897c
Gareth <gjoyce@iweb.co.uk>
2015-11-12 16:04:43 +0000

    Removed old js file that dealt with nav and script link.

--- static/inc/scripts.php
+++ static/inc/scripts.php
@@ -2,9 +2,7 @@
 <script src="<?php echo getUrl('../build/bower_components/jquery/dist/jquery.min.js'); ?>"></script>
 <script src="<?php echo getUrl('../build/bower_components/picturefill/dist/picturefill.min.js'); ?>"></script>
 <script src="<?php echo getUrl('../build/js/script.js'); ?>"></script>
-<!-- <script src="<?php echo getUrl('../build/js/chopchop.js'); ?>"></script> -->
 <script src="<?php echo getUrl('../build/js/chopchop.lib.js'); ?>"></script>
-
 <script type="text/javascript">
     (function() {
         var toggle = new ChopChop.Toggle('body');
