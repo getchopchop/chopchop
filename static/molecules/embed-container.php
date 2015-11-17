a2890441bb1a1bde9a6c8ddfdd3caf19fed47aec
Dave Redfern <dave@daveredfern.com>
2015-11-17 09:29:23 +0000

    Fixing classes on library for video embed by adding u- infront

--- static/molecules/embed-container.php
+++ static/molecules/embed-container.php
@@ -1,13 +1,15 @@
 <div class="u-container">
+
     <h2>16x9 Embed container (default)</h2>
     <p>The default aspect ration of an embed container is 16:9.</p>
-    <div class="embed-container">
+    <div class="u-embed-container">
         <iframe src="//www.youtube.com/embed/qrO4YZeyl0I" frameborder="0" allowfullscreen=""></iframe>
     </div>
 
     <h2>4x3 modifier</h2>
     <p>Using the modifier <code>embed-container--4x3</code> changes the aspect ratio to 4:3.</p>
-    <div class="embed-container embed-container--4x3">
+    <div class="u-embed-container u-embed-container--4x3">
         <iframe src="//www.youtube.com/embed/qrO4YZeyl0I" frameborder="0" allowfullscreen=""></iframe>
     </div>
+    
 </div>
