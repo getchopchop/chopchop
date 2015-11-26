88df5b0740c38a42b3eac15575141108ab566bd6
Dave Redfern <dave@daveredfern.com>
2015-11-26 16:04:42 +0000

    Adding new logos by default

--- static/branding/logo.php
+++ static/branding/logo.php
@@ -1,74 +1,90 @@
 <div class="u-container">
-    
+
     <h2>Logo Usage</h2>
-    
+
     <h3>Full Logo</h3>
-    <img src="/src/img/logo.png" class="u-img-responsive" />
-    
+    <p><img src="<?php echo getUrl('../build/img/logos/logo.png'); ?>" class="u-img-responsive" /></p>
+
+    <h3>Logo variations</h3>
+    <div class="g-two-up g-gutter">
+        <div>
+            <h4>Print logo</h4>
+            <img src="<?php echo getUrl('../build/img/logo_print.gif'); ?>" class="u-img-responsive" />
+        </div>
+        <div>
+            <h4>Email logo</h4>
+            <img src="<?php echo getUrl('../build/img/logo_email.gif'); ?>" class="u-img-responsive" />
+        </div>
+    </div>
+
     <h3>Apple Touch Favicons</h3>
     <ul class="g-four-up u-list-unstyled">
         <li>
-            <img src="/src/img/favicons/apple-touch-icon-180x180.png" class="u-img-responsive" />
+            <img src="<?php echo getUrl('../build/vendor/favicons/apple-touch-icon-180x180.png'); ?>" class="u-img-responsive" />
             <span>180x180</span>
         </li>
         <li>
-            <img src="/src/img/favicons/apple-touch-icon-152x152.png" class="u-img-responsive" />
+            <img src="<?php echo getUrl('../build/vendor/favicons/apple-touch-icon-152x152.png'); ?>" class="u-img-responsive" />
             <span>152x152</span>
         </li>
         <li>
-            <img src="/src/img/favicons/apple-touch-icon-114x114.png" class="u-img-responsive" />
+            <img src="<?php echo getUrl('../build/vendor/favicons/apple-touch-icon-114x114.png'); ?>" class="u-img-responsive" />
             <span>114x114</span>
         </li>
         <li>
-            <img src="/src/img/favicons/apple-touch-icon-76x76.png" class="u-img-responsive" />
+            <img src="<?php echo getUrl('../build/vendor/favicons/apple-touch-icon-76x76.png'); ?>" class="u-img-responsive" />
             <span>76x76</span>
         </li>
         <li>
-            <img src="/src/img/favicons/apple-touch-icon-72x72.png" class="u-img-responsive" />
+            <img src="<?php echo getUrl('../build/vendor/favicons/apple-touch-icon-72x72.png'); ?>" class="u-img-responsive" />
             <span>72x72</span>
         </li>
         <li>
-            <img src="/src/img/favicons/apple-touch-icon-60x60.png" class="u-img-responsive" />
+            <img src="<?php echo getUrl('../build/vendor/favicons/apple-touch-icon-60x60.png'); ?>" class="u-img-responsive" />
             <span>60x60</span>
         </li>
-        
+
         <li>
-            <img src="/src/img/favicons/apple-touch-icon-57x57.png" class="u-img-responsive" />
+            <img src="<?php echo getUrl('../build/vendor/favicons/apple-touch-icon-57x57.png'); ?>" class="u-img-responsive" />
             <span>57x57</span>
         </li>
     </ul>
-    
+
     <h3>Favicons</h3>
     <ul class="g-four-up u-list-unstyled">
         <li>
-            <img src="/src/img/favicons/favicon-194x194.png" class="u-img-responsive" />
+            <img src="<?php echo getUrl('../build/vendor/favicons/favicon-194x194.png'); ?>" class="u-img-responsive" />
             <span>194x194</span>
         </li>
         <li>
-            <img src="/src/img/favicons/favicon-96x96.png" class="u-img-responsive" />
+            <img src="<?php echo getUrl('../build/vendor/favicons/favicon-96x96.png'); ?>" class="u-img-responsive" />
             <span>96x96</span>
         </li>
         <li>
-            <img src="/src/img/favicons/favicon-32x32.png" class="u-img-responsive" />
+            <img src="<?php echo getUrl('../build/vendor/favicons/favicon-32x32.png'); ?>" class="u-img-responsive" />
             <span>32x32</span>
         </li>
         <li>
-            <img src="/src/img/favicons/favicon-16x16.png" class="u-img-responsive" />
+            <img src="<?php echo getUrl('../build/vendor/favicons/favicon-16x16.png'); ?>" class="u-img-responsive" />
             <span>16x16</span>
         </li>
     </ul>
-    
+
     <div class="g-two-up">
         <div class="well">
-            <h3>Android Home Screen</h3>
-            <img src="/src/img/favicons/android-chrome-192x192.png" class="u-img-responsive" />
+            <h4>Android Home Screen</h4>
+            <img src="<?php echo getUrl('../build/vendor/favicons/android-chrome-192x192.png'); ?>" class="u-img-responsive" />
             <span>192x192</span>
         </div>
         <div class="bg-blue well">
-            <h3>Windows Tile (Reverse)</h3>
-            <img src="/src/img/favicons/mstile-144x144.png" class="u-img-responsive" />
+            <h4>Windows Tile (Reverse)</h4>
+            <img src="<?php echo getUrl('../build/vendor/favicons/mstile-144x144.png'); ?>" class="u-img-responsive" />
             <span>144x144</span>
         </div>
     </div>
-    
+
+    <h3>Placeholder</h3>
+    <p><img src="<?php echo getUrl('../build/img/logos/placeholder.jpg'); ?>" class="u-img-responsive" /></p>
+
+
 </div>
