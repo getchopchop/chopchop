ad8e94c27d5a899ea7e802b8341ddb1b185e2b7b
Dave Redfern <dave@daveredfern.com>
2015-12-04 14:52:40 +0000

    Adding cards folder to test

---
+++ static/molecules/card/1-simple.php
@@ -0,0 +1,11 @@
+<div class="card">
+    <div class="card__header">
+        Card Header
+    </div>
+    <div class="card__body">
+        Card Body
+    </div>
+    <div class="card__footer">
+        Card Footer
+    </div>
+</div>
