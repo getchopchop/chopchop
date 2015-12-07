7cb0ad59ea255147c3d019756e65728cc5160e94
Dave Redfern <dave@daveredfern.com>
2015-12-07 09:00:27 +0000

    Moving logos all into same folder

--- static/branding/logo/2-variation.php
+++ static/branding/logo/2-variation.php
@@ -1,10 +1,10 @@
 <div class="g-two-up g-gutter">
     <div>
         <h4>Print logo</h4>
-        <img src="<?php echo getUrl('../build/img/logo_print.gif'); ?>" class="u-img-responsive" />
+        <img src="<?php echo getUrl('../build/img/logos/logo_print.gif'); ?>" class="u-img-responsive" />
     </div>
     <div>
         <h4>Email logo</h4>
-        <img src="<?php echo getUrl('../build/img/logo_email.gif'); ?>" class="u-img-responsive" />
+        <img src="<?php echo getUrl('../build/img/logos/logo_email.gif'); ?>" class="u-img-responsive" />
     </div>
 </div>
