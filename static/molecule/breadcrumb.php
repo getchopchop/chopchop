2ee375dcc6c979a29083e284b065b01058f5c8b4
Dave Redfern <dave@daveredfern.com>
2015-12-10 20:39:28 +0000

    Amends to breadcrumbs

---
+++ static/molecule/breadcrumb.php
@@ -0,0 +1,12 @@
+<?php /*
+Title: Breadcrumb
+Class: .breadcrumb
+*/ ?>
+
+<nav class="breadcrumb">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li>Company Information</li>
+    </ul>
+</nav>
