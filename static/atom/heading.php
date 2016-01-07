dbf52bbd60bbb54e57f17f2539657fb39065112d
Dave Redfern <dave@daveredfern.com>
2016-01-07 16:31:30 +0000

    Adding heading p styles

--- static/atom/heading.php
+++ static/atom/heading.php
@@ -3,8 +3,19 @@ Title: Headings
 */ ?>
 
 <h1>h1 Heading</h1>
+<p class="h1">h1 Heading</p>
+
 <h2>h2 Heading</h2>
+<p class="h2">h2 Heading</p>
+
 <h3>h3 Heading</h3>
+<p class="h3">h3 Heading</p>
+
 <h4>h4 Heading</h4>
+<p class="h4">h4 Heading</p>
+
 <h5>h5 Heading</h5>
+<p class="h5">h5 Heading</p>
+
 <h6>h6 Heading</h6>
+<p class="h6">h6 Heading</p>
