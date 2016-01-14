66c531b33010db1393a30132228e7eb09804a0e5
Dave Redfern <dave@daveredfern.com>
2016-01-14 13:52:44 +0000

    Removing reference to print stylesheet

--- static/inc/styles.php
+++ static/inc/styles.php
@@ -2,6 +2,5 @@
 <link rel="stylesheet" type="text/css" href="<?php echo getUrl('build/bower_components/normalize.css/normalize.css'); ?>" media="all" />
 <link rel="stylesheet" type="text/css" href="<?php echo getUrl('build/vendor/fontello/css/fontello.css'); ?>" media="all" />
 <link rel="stylesheet" type="text/css" href="<?php echo getUrl('build/css/styles.css'); ?>" media="screen" />
-<link rel="stylesheet" type="text/css" href="<?php echo getUrl('build/css/print.css'); ?>" media="print" />
 <link rel="stylesheet" type="text/css" href="<?php echo getUrl('build/css/chopchop-ui.css'); ?>" media="screen" />
 <!-- End template: _styles !-->
