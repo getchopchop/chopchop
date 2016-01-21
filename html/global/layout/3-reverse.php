7bd454d01b39becf1bd9399ddccb3ec96edee667
Dave Redfern <dave@daveredfern.com>
2016-01-21 15:56:08 +0000

    Adding layout

---
+++ html/global/layout/3-reverse.php
@@ -0,0 +1,9 @@
+<?php /*
+Title: Reverse layout
+Class: .l-reverse
+*/ ?>
+
+<div class="l-two-col l-reverse">
+    <div class="l-side"><div class="cc-box">Unit 1</div></div>
+    <div class="l-body"><div class="cc-box">Unit 2</div></div>
+</div>
