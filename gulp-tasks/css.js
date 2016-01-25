1674e963deb71d24a4f07c69af941340d27a46ae
Jake Cobley <jake@iweb.co.uk>
2016-01-25 09:06:00 +0000

    rearranged pipes in the css task to prevent errors in the future

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -10,9 +10,9 @@ module.exports = function (gulp, nodeModule, path, option, environment) {
         .pipe(environment.dev ? nodeModule.sourcemaps.init() : nodeModule.util.noop())
         .pipe(nodeModule.sass.sync().on('error', nodeModule.sass.logError))
         .pipe(nodeModule.autoPrefixer(option.autoprefixer))
-        .pipe(environment.dev ? nodeModule.sourcemaps.write() : nodeModule.util.noop())
         .pipe(environment.production ? nodeModule.combineMq() : nodeModule.util.noop())
         .pipe(environment.production ? nodeModule.cssNano() : nodeModule.util.noop())
+        .pipe(environment.dev ? nodeModule.sourcemaps.write() : nodeModule.util.noop())
         .pipe(gulp.dest(path.styles.build))
         .pipe(environment.dev ? nodeModule.browserSync.reload({stream: true}) : nodeModule.util.noop());
     };
