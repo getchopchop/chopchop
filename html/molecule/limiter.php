1addbe74b6950c7b289fbd838c29a0beea5dba92
Gareth Joyce <gjoyce@iweb.co.uk>
2016-02-02 16:09:48 +0000

    added limiter and sorter and rating.

---
+++ html/molecule/limiter.php
@@ -0,0 +1,13 @@
+<?php /*
+Title: Limiter
+*/ ?>
+
+<div class="limiter">
+    <label class="u-visually-hidden">Show</label>
+    <select>
+        <option value="?limit=24">24 per page</option>
+        <option value="?limit=48" selected="selected">48 per page</option>
+        <option value="?limit=96">96 per page</option>
+        <option value="?limit=all">All per page</option>
+    </select>
+</div>
