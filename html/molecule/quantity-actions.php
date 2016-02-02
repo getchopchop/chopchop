f90700399f8e8fce3992ce28bfb7a76a3f545716
Gareth Joyce <gjoyce@iweb.co.uk>
2016-02-02 16:37:27 +0000

    Added PAyment icons, pricing, quantity actions.

---
+++ html/molecule/quantity-actions.php
@@ -0,0 +1,20 @@
+<?php /*
+Title: Quantity
+*/ ?>
+
+<div class="qty-actions">
+    <span class="qty-actions__label">Quantity</span>
+    <div class="qty-actions__scale">
+        <a href="#" class="btn btn--xs btn--minus">
+            <i class="shape"></i>
+            <span class="btn__text">Decrease quantity</span>
+        </a>
+        <span class="qty-actions__qty">
+            <input type="number" min="0" class="qty" placeholder="Qty">
+        </span>
+        <a href="#" class="btn btn--xs btn--plus">
+            <i class="shape"></i>
+            <span class="btn__text">Increase quantity</span>
+        </a>
+    </div>
+</div>
