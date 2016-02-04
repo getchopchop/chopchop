8b36726cd65c3dafd932ba6ebdf8e15c17598351
Dave Redfern <dave@daveredfern.com>
2016-02-04 15:01:01 +0000

    Don't hate me gareth, ocd

--- html/molecule/quantity.php
+++ html/molecule/quantity.php
@@ -1,17 +1,18 @@
 <?php /*
 Title: Quantity
+Class: .quantity
 */ ?>
 
-<div class="qty-actions">
+<div class="quantity">
     <label>Quantity</label>
-    <div>
+    <div class="quantity__amount">
         <a href="#" class="btn btn--xs">
-            <i class="a-plus"></i>
+            <i class="icon-plus"></i>
             <span class="u-visually-hidden">Decrease quantity</span>
         </a>
-        <input type="number" min="0" class="qty" placeholder="Qty">
+        <input type="number" min="0" class="u-text-center" placeholder="Qty">
         <a href="#" class="btn btn--xs">
-            <i class="a-minus"></i>
+            <i class="icon-minus"></i>
             <span class="u-visually-hidden">Increase quantity</span>
         </a>
     </div>
