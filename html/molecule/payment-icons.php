9ee98913b4b6f9f1cef32adc1fa5b8997c3ed28c
Dave Redfern <dave@daveredfern.com>
2016-02-04 14:33:23 +0000

    Amends to css to us ultils

--- html/molecule/payment-icons.php
+++ html/molecule/payment-icons.php
@@ -3,5 +3,9 @@ Title: Payment Images
 */ ?>
 
 <ul class="u-list-inline payment-icons">
-    <li><img src="<?php echo getUrl('build/img/icons/payments/color/sage.svg'); ?>" alt="SagePay" height="40" class="payment-icons__img" /></li><li><img src="<?php echo getUrl('build/img/icons/payments/color/paypal.svg'); ?>" alt="PayPal" height="40" class="payment-icons__img" /></li><li><img src="<?php echo getUrl('build/img/icons/payments/color/visa.svg'); ?>" alt="Visa" height="40" class="payment-icons__img" /></li><li><img src="<?php echo getUrl('build/img/icons/payments/color/maestro.svg'); ?>" alt="Maestro" height="40" class="payment-icons__img" /></li><li><img src="<?php echo getUrl('build/img/icons/payments/color/mastercard.svg'); ?>" alt="Mastercard" height="40" class="payment-icons__img" /></li><li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/color/sage.svg'); ?>" alt="SagePay" height="40" class="payment-icons__img" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/color/paypal.svg'); ?>" alt="PayPal" height="40" class="payment-icons__img" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/color/visa.svg'); ?>" alt="Visa" height="40" class="payment-icons__img" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/color/maestro.svg'); ?>" alt="Maestro" height="40" class="payment-icons__img" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/color/mastercard.svg'); ?>" alt="Mastercard" height="40" class="payment-icons__img" /></li>
 </ul>
--- html/molecule/quantity-actions.php
+++ html/molecule/quantity-actions.php
@@ -3,18 +3,18 @@ Title: Quantity
 */ ?>
 
 <div class="qty-actions">
-    <span class="qty-actions__label">Quantity</span>
-    <div class="qty-actions__scale">
-        <a href="#" class="btn btn--xs btn--minus">
-            <i class="shape"></i>
-            <span class="btn__text">Decrease quantity</span>
+    <label>Quantity</label>
+    <div>
+        <a href="#" class="btn btn--xs">
+            <i class="a-plus"></i>
+            <span class="u-visually-hidden">Decrease quantity</span>
         </a>
         <span class="qty-actions__qty">
             <input type="number" min="0" class="qty" placeholder="Qty">
         </span>
-        <a href="#" class="btn btn--xs btn--plus">
-            <i class="shape"></i>
-            <span class="btn__text">Increase quantity</span>
+        <a href="#" class="btn btn--xs">
+            <i class="a-minus"></i>
+            <span class="u-visually-hidden">Increase quantity</span>
         </a>
     </div>
 </div>
