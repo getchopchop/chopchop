99cf73189a22d7eee3c091147b96245d816adc99
Dave Redfern <dave@daveredfern.com>
2016-03-04 09:15:39 +0000

    merge

---
+++ html/utility/container/2-container-sm.php
@@ -0,0 +1,10 @@
+<?php /*
+Title: Small Container
+Class: .u-container.u-container--sm
+*/ ?>
+
+<div class="u-container u-container--sm">
+    <div class="cc-box">
+        I am in a small container
+    </div>
+</div>
