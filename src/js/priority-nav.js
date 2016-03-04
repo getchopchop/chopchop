14d0e214980899bf85f6467ed97e790b19888f95
Dave Redfern <dave@daveredfern.com>
2016-03-04 09:14:46 +0000

    Better but still buggy priority nav

---
+++ src/js/priority-nav.js
@@ -0,0 +1,58 @@
+;(function($){
+
+    $(document).ready(function() {
+
+        // =============================================
+        // Priority nav
+        // =============================================
+
+        var $nav = $('.priority-nav__container');
+        var $btn = $('.priority-nav__toggle');
+        var $vlinks = $('.priority-nav__nav');
+        var $hlinks = $('.priority-nav__sub-nav');
+
+        var numOfItems = 0;
+        var totalSpace = 0;
+        var breakWidths = [];
+
+        // Get initial state
+        $vlinks.children().outerWidth(function(i, w) {
+            totalSpace += w;
+            numOfItems += 1;
+            breakWidths.push(totalSpace);
+        });
+
+        var availableSpace, numOfVisibleItems, requiredSpace;
+
+        function check() {
+            // Get instant state
+            availableSpace = $vlinks.width() - 10;
+            numOfVisibleItems = $vlinks.children().length;
+            requiredSpace = breakWidths[numOfVisibleItems - 1];
+
+            // There is not enought space
+            if (requiredSpace > availableSpace) {
+                $vlinks.children().last().prependTo($hlinks);
+                numOfVisibleItems -= 1;
+                check();
+            // There is more than enough space
+            } else if (availableSpace > breakWidths[numOfVisibleItems]) {
+                $hlinks.children().first().appendTo($vlinks);
+                numOfVisibleItems += 1;
+            }
+
+            // Update the button accordingly
+            $btn.attr("count", numOfItems - numOfVisibleItems);
+            if (numOfVisibleItems === numOfItems) {
+                $btn.removeClass('is-active');
+            } else $btn.addClass('is-active');
+        }
+
+        // Window listeners
+        $(window).resize(function() {
+            check();
+        });
+        check();
+
+    });
+}(jQuery));
