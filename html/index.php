1abd530df3f6695311451eac0df3319a2df969f4
Gareth Joyce <gjoyce@iweb.co.uk>
2016-03-24 08:41:38 +0000

    CC header.

--- html/index.php
+++ html/index.php
@@ -25,8 +25,8 @@
 
         <main>
               <?php if(isIndex()) { ?>
-                  <header class="header">
-                      <div class="u-container u-text-center">
+                  <header class="cc-header">
+                      <div class="u-container">
                           <?php include('molecule/logo.php'); ?>
                       </div>
                   </header>
