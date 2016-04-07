27ad8b05909f371e230e7f2cbe5fa7ab87d15b6a
Dave Redfern <dave@daveredfern.com>
2016-04-07 16:01:28 +0100

    Sorting out homepage

--- html/inc/nav-index.php
+++ html/inc/nav-index.php
@@ -2,13 +2,14 @@
     <?php
     foreach($toplevels as $toplevel) {
     ?>
+    <?php if($toplevel == 'atom') { echo '</div><div class="cc-index-nav">'; } ?>
     <div>
-        <div class="nav-<?= $toplevel ?> cc-index-nav__item">
+        <div class="card card--bordered nav-<?= $toplevel ?> cc-index-nav__item">
             <div class="card__header cc-index-nav__header has-plus-minus" data-cc-action="toggle" data-cc-target="toggle-<?= $toplevel ?>" id="toggle-title-<?= $toplevel ?>" data-cc-group="toggle-titles">
                 <h4><?= ucwords($toplevel) ?></h4>
                 <span class="shape"></span>
             </div>
-            <div class="cc-index-nav__body u-toggle" id="toggle-<?= $toplevel ?>" data-cc-cascade="toggle-title-<?= $toplevel ?>" data-cc-group="toggle-contents">
+            <div class="card__body cc-index-nav__body u-toggle" id="toggle-<?= $toplevel ?>" data-cc-cascade="toggle-title-<?= $toplevel ?>" data-cc-group="toggle-contents">
                 <ul class="nav nav--space child">
                     <?php
                     $quarks = dir($toplevel);
