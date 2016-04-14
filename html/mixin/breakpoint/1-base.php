044a159d1bbc3f60fe60db20006df4df2472580b
Dave Redfern <dave@daveredfern.com>
2016-04-14 19:04:44 +0100

    Adding breakpoint mixin html

---
+++ html/mixin/breakpoint/1-base.php
@@ -0,0 +1,9 @@
+<?php /*
+Title: Breakpoint Mixin
+Class: @include breakpoint($min-width);
+*/ ?>
+
+<div class="grid">
+    <div><div class="cc-box u-visible-sm">Visible for small</div></div>
+    <div><div class="cc-box u-hidden-sm">Hidden for small</div></div>
+</div>
---
+++ html/mixin/breakpoint/1-min-max.php
@@ -0,0 +1,9 @@
+<?php /*
+Title: Breakpoint Mixin using min and max
+Class: @include breakpoint($min-width, $max-width);
+*/ ?>
+
+<div class="grid">
+    <div><div class="cc-box u-visible-sm">Visible for small</div></div>
+    <div><div class="cc-box u-hidden-sm">Hidden for small</div></div>
+</div>
