0cae78b5718df85066d030044bf2e320f37e968f
Dave Redfern <dave@daveredfern.com>
2016-04-14 18:56:14 +0100

    Deleting layout and adding mixin html

---
+++ html/mixin/grid/4-center.php
@@ -0,0 +1,8 @@
+<?php /*
+Title: Grid Center Mixin
+Class: @include grid-center;
+*/ ?>
+
+<div class="grid g-gutter">
+    <div class="g-col-xs-6"><div class="cc-box">Box</div></div>
+</div>
---
+++ html/mixin/grid/6-justify.php
@@ -0,0 +1,9 @@
+<?php /*
+Title: Grid Center Mixin
+Class: @include grid-center;
+*/ ?>
+
+<div class="grid g-justify">
+    <div class="g-col-xs-4"><div class="cc-box">1</div></div>
+    <div class="g-col-xs-4"><div class="cc-box">2</div></div>
+</div>
