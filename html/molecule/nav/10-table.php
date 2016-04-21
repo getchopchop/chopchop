95009fceb7200074864a795fc201766dcd2e6834
Dave Redfern <dave@daveredfern.com>
2016-04-21 16:42:05 +0100

    Making navs mixins

--- html/molecule/nav/10-table.php
+++ html/molecule/nav/10-table.php
@@ -1,10 +1,10 @@
 <?php /*
-Title: Inline Pills Nav
-Class: .nav.nav--inline.nav--pills
+Title: Table Nav
+Class: .nav nav--table
 */ ?>
 
 <nav>
-    <ul class="nav nav--inline nav--pills">
+    <ul class="nav nav--table">
         <li class="nav__item"><a href="#">Overview</a></li>
         <li class="nav__item"><a href="#">Credits</a></li>
         <li class="nav__item"><a href="#">Structure</a></li>
