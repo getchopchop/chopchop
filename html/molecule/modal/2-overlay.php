67deffcdf50cbdf018dc736be66e0e7033332edf
Gareth Joyce <gjoyce@iweb.co.uk>
2016-04-28 15:24:13 +0100

    split out modals for chop chop demo.

---
+++ html/molecule/modal/2-overlay.php
@@ -0,0 +1,7 @@
+<?php /*
+Title: Overlay
+Class: .overlay
+Description: The overlay is hidden off screen by default, it shows when the modals are active.
+*/ ?>
+
+<div class="overlay" id="overlay" data-cc-action="deactivate" data-cc-target="modal"></div>
