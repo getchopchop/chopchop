acd9dd655f65853dddd460c79a2d18fb8ae3055b
Dave Redfern <dave@daveredfern.com>
2016-06-09 16:06:21 +0100

    Reorganising svg and removing fontello

--- html/inc/styles.php
+++ html/inc/styles.php
@@ -1,6 +1,5 @@
 <!-- Template: _styles !-->
 <link rel="stylesheet" type="text/css" href="<?php echo getUrl('build/vendor/normalize.css/normalize.css'); ?>" media="all" />
-<link rel="stylesheet" type="text/css" href="<?php echo getUrl('build/vendor/fontello/css/fontello.css'); ?>" media="all" />
 <link rel="stylesheet" type="text/css" href="<?php echo getUrl('build/css/styles.css'); ?>" media="screen" />
 <script src="<?php echo getUrl('build/vendor/picturefill/picturefill.js'); ?>"></script>
 
