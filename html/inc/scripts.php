8910087f498f06ce4bc2426f54ca70d122a0d124
Gareth Joyce <gjoyce@iweb.co.uk>
2016-06-17 10:21:28 +0100

    Testing: svg4everybody.

--- html/inc/scripts.php
+++ html/inc/scripts.php
@@ -1,5 +1,6 @@
 <!-- Template: _js !-->
 <script src="<?php echo getUrl('build/vendor/jquery/jquery.js'); ?>"></script>
+<script src="<?php echo getUrl('build/vendor/svg4everybody/svg4everybody.js'); ?>"></script>
 <script src="<?php echo getUrl('build/js/chopchop.lib.js'); ?>"></script>
 <script src="<?php echo getUrl('build/js/priority-nav.js'); ?>"></script>
 <script src="<?php echo getUrl('build/js/script.js'); ?>"></script>
