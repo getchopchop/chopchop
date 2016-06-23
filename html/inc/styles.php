4f280c56a167144a2371c16c996831d5946e23e2
Gareth Joyce <gjoyce@iweb.co.uk>
2016-06-23 15:40:34 +0100

    Added async to relevant scripts and moved js to bottom.

--- html/inc/styles.php
+++ html/inc/styles.php
@@ -1,8 +1,6 @@
 <!-- Template: _styles !-->
 <link rel="stylesheet" type="text/css" href="<?php echo getUrl('build/vendor/normalize.css/normalize.css'); ?>" media="all" />
 <link rel="stylesheet" type="text/css" href="<?php echo getUrl('build/css/styles.css'); ?>" media="screen" />
-<script src="<?php echo getUrl('build/vendor/picturefill/picturefill.js'); ?>"></script>
-
 <?php if(!isset($_GET['preview'])) : ?>
     <link rel="stylesheet" type="text/css" href="<?php echo getUrl('build/css/chopchop-ui.css'); ?>" media="screen" />
 <?php endif; ?>
