0929332f7c0cad2fb066d52ab46451fce356143b
Gareth Joyce <gjoyce@iweb.co.uk>
2016-07-07 15:31:34 +0100

    Section Utility Added. Ref: Issue #23.

---
+++ html/utility/section/1-base.php
@@ -0,0 +1,10 @@
+<?php /*
+Title: Section
+Class: .u-section
+*/ ?>
+
+<div class="u-clearfix u-fill-grey-lighter">
+    <div class="u-section">
+        <p>I am in a section with margin top and bottom</p>
+    </div>
+</div>
---
+++ html/utility/section/3-large.php
@@ -0,0 +1,10 @@
+<?php /*
+Title: Section
+Class: .u-section-lg
+*/ ?>
+
+<div class="u-clearfix u-fill-grey-lighter">
+    <div class="u-section-lg">
+        <p>I am in a section with a large margin top and bottom</p>
+    </div>
+</div>
