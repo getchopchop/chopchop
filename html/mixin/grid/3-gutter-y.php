ddf6b466a0f4c7b92527d11b39c820cd01cc9712
Dave Redfern <dave@daveredfern.com>
2016-07-07 15:31:06 +0100

    Adding grid gutters for x and y.

---
+++ html/mixin/grid/3-gutter-y.php
@@ -0,0 +1,11 @@
+<?php /*
+Title: Grid Gutter Y Mixin
+Class: @include grid-gutter-y($gutter);
+*/ ?>
+
+<div class="grid g-gutter-y">
+    <div class="g-col-xs-6"><div class="cc-box">Box</div></div>
+    <div class="g-col-xs-6"><div class="cc-box">Box</div></div>
+    <div class="g-col-xs-6"><div class="cc-box">Box</div></div>
+    <div class="g-col-xs-6"><div class="cc-box">Box</div></div>
+</div>
--- html/utility/grid/5-gutter.php
+++ html/utility/grid/5-gutter.php
@@ -1,9 +1,13 @@
 <?php /*
-Title: Grid
+Title: Grid Gutter
 Class: .g-gutter
 */ ?>
 
 <div class="grid g-gutter">
+    <div class="g-col-xs-3"><div class="cc-box"><code>.g-col-xs-3</code></div></div>
+    <div class="g-col-xs-3"><div class="cc-box"><code>.g-col-xs-3</code></div></div>
+    <div class="g-col-xs-3"><div class="cc-box"><code>.g-col-xs-3</code></div></div>
+    <div class="g-col-xs-3"><div class="cc-box"><code>.g-col-xs-3</code></div></div>
     <div class="g-col-xs-3"><div class="cc-box"><code>.g-col-xs-3</code></div></div>
     <div class="g-col-xs-3"><div class="cc-box"><code>.g-col-xs-3</code></div></div>
     <div class="g-col-xs-3"><div class="cc-box"><code>.g-col-xs-3</code></div></div>
