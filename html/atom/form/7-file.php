e5f7d783d57354df34767f01bf1588f1b1034065
Gareth Joyce <gjoyce@iweb.co.uk>
2016-07-14 14:22:12 +0100

    Added file input type and pattern to custom style it.

---
+++ html/atom/form/7-file.php
@@ -0,0 +1,12 @@
+<?php /*
+Title: File
+*/ ?>
+
+<fieldset>
+    <div class="field-group">
+        <label class="field-group__label btn" for="file">Choose a file</label>
+        <div class="field-group__field u-visually-hidden">
+            <input type="file" name="file" id="file" />
+        </div>
+    </div>
+</fieldset>
