caaf602b77773ee55c395f4f6e2ddf7191cd0c02
Dave Redfern <dave@daveredfern.com>
2016-07-21 15:08:14 +0100

    #29 Improving typography out the box

--- html/global/typography/1-font.php
+++ html/global/typography/1-font.php
@@ -3,10 +3,6 @@ Title: Font
 Class: .u-font-family
 */ ?>
 
-<div>
-    $font-family: Helmet, Freesans, Helvetica, Arial, sans-serif; font-weight: 300;
-</div>
-<hr />
 <div class="u-font-family">
     a b c d e f g h i j k l m n o p q r s t u v w x y z <br>
     A B C D E F G H I J K L M N O P Q R S T U V W X Y Z <br>
