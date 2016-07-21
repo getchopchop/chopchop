8b62ae4626b9de3e6462cdf93deff8d3eccd792b
Dave Redfern <dave@daveredfern.com>
2016-07-21 14:42:01 +0100

    Fixing cards throughout the interface

--- html/global/color.php
+++ html/global/color.php
@@ -16,13 +16,13 @@ Title: Colour
             $group = $bits[2];
             ?>
             <?php if($group != $previous_group) {
-                ?><h1><?php echo $group ?></h1><?php
+                ?><div class="g-col-xs-12"><h3><?php echo $group ?></h3></div><?php
                 $previous_group = $group;
             }
             ?>
             <div class="g-col-xs-6 g-col-sm-4 g-col-md-3 g-col-xl-2">
                 <div class="card cc-card--swatch">
-                    <div class="card__body <?php echo $class_ ?>">
+                    <div class="u-block-xl <?php echo $class_ ?>">
 
                     </div>
                     <div class="card__footer">
