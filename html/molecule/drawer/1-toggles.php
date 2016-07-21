3727aacc6dd2056112cfe9292dca3aef1376d46e
Gareth Joyce <gjoyce@iweb.co.uk>
2016-07-21 14:43:33 +0100

    #36 bugfix on drawers, also updated demo cards to use new block utility.

--- html/molecule/drawer/1-toggles.php
+++ html/molecule/drawer/1-toggles.php
@@ -3,4 +3,4 @@ Title: Drawer Toggles
 */ ?>
 
 <p><a href="#" data-cc-target="drawer-left-info" data-cc-action="activate">Left Drawer Info</a></p>
-<p><a href="#" data-cc-target="drawer-right" data-cc-action="activate">Right drawer</a></p>
+<p><a href="#" data-cc-target="drawer-right-info" data-cc-action="activate">Right Drawer Info</a></p>
