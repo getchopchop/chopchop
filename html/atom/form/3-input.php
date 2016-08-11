51435b1a59bc3a71d47076db1af9d1e9ccd78a77
Dave Redfern <dave@daveredfern.com>
2016-08-11 16:17:48 +0100

    #9 Fixing range input output on IE

--- html/atom/form/3-input.php
+++ html/atom/form/3-input.php
@@ -55,7 +55,7 @@ Title: Inputs
     <label class="field-group__label">Range input</label>
     <div class="field-group__field">
         <input type="range" min="0" max="100" value="50" id="fader" step="1" oninput="outputUpdate(value)">
-        <output for="fader" id="volume">50</output>
+        <output for="fader" id="volume"></output>
         <script>
             function outputUpdate(vol) {
                 document.querySelector('#volume').value = vol;
