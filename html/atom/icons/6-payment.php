ae168a2b41d08124a2efe054d47540153ccdadca
Jake Cobley <jake@cobe.ly>
2016-08-15 13:59:20 +0100

    Split SVG sprite into multiple files (#57)
    Generates svg sprites per top level directory within img/icons instead of a single sprite. Helps prevent file sizes getting too large.
    
    - Updated Gulpfile to run svg-sprite task per top level directory within img/icons
    - Edited the printSvg function to work with the new directory structure
    
    Resolves: issue #18

--- html/atom/icons/6-payment.php
+++ html/atom/icons/6-payment.php
@@ -3,67 +3,67 @@ Title: Payment Icons using images
 */ ?>
 
 <ul class="u-list-inline">
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--amazon.svg'); ?>" alt="Amazon" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--american-express.svg'); ?>" alt="America Express" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--braintree.svg'); ?>" alt="Braintree" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--cirrus.svg'); ?>" alt="Cirrus" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--direct-debit.svg'); ?>" alt="Direct Debit" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--ebay.svg'); ?>" alt="Ebay" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--jcb.svg'); ?>" alt="JCB" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--maestro.svg'); ?>" alt="Maestro" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--mastercard-securecode.svg'); ?>" alt="Mastercard Securecode" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--mastercard.svg'); ?>" alt="Mastercard" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--norton.svg'); ?>" alt="Norton" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--paypal.svg'); ?>" alt="Paypal" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--sage.svg'); ?>" alt="Sage" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--stripe.svg'); ?>" alt="Stripe" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--verified-by-visa.svg'); ?>" alt="Verified By Visa" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--visa-electron.svg'); ?>" alt="Visa Electron" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--visa.svg'); ?>" alt="Visa" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--western-union.svg'); ?>" alt="Western Union" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--color--worldpay.svg'); ?>" alt="WordPay" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--amazon.svg'); ?>" alt="Amazon" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--american-express.svg'); ?>" alt="America Express" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--braintree.svg'); ?>" alt="Braintree" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--cirrus.svg'); ?>" alt="Cirrus" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--direct-debit.svg'); ?>" alt="Direct Debit" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--ebay.svg'); ?>" alt="Ebay" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--jcb.svg'); ?>" alt="JCB" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--maestro.svg'); ?>" alt="Maestro" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--mastercard-securecode.svg'); ?>" alt="Mastercard Securecode" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--mastercard.svg'); ?>" alt="Mastercard" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--norton.svg'); ?>" alt="Norton" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--paypal.svg'); ?>" alt="Paypal" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--sage.svg'); ?>" alt="Sage" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--stripe.svg'); ?>" alt="Stripe" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--verified-by-visa.svg'); ?>" alt="Verified By Visa" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--visa-electron.svg'); ?>" alt="Visa Electron" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--visa.svg'); ?>" alt="Visa" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--western-union.svg'); ?>" alt="Western Union" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/color--worldpay.svg'); ?>" alt="WordPay" width="63" height="40" /></li>
 </ul>
 
 <ul class="u-list-inline">
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--amazon.svg'); ?>" alt="Amazon" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--american-express.svg'); ?>" alt="America Express" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--braintree.svg'); ?>" alt="Braintree" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--cirrus.svg'); ?>" alt="Cirrus" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--direct-debit.svg'); ?>" alt="Direct Debit" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--ebay.svg'); ?>" alt="Ebay" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--jcb.svg'); ?>" alt="JCB" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--maestro.svg'); ?>" alt="Maestro" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--mastercard-securecode.svg'); ?>" alt="Mastercard Securecode" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--mastercard.svg'); ?>" alt="Mastercard" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--norton.svg'); ?>" alt="Norton" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--paypal.svg'); ?>" alt="Paypal" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--sage.svg'); ?>" alt="Sage" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--stripe.svg'); ?>" alt="Stripe" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--verified-by-visa.svg'); ?>" alt="Verified By Visa" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--visa-electron.svg'); ?>" alt="Visa Electron" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--visa.svg'); ?>" alt="Visa" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--western-union.svg'); ?>" alt="Western Union" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--white--worldpay.svg'); ?>" alt="WordPay" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--amazon.svg'); ?>" alt="Amazon" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--american-express.svg'); ?>" alt="America Express" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--braintree.svg'); ?>" alt="Braintree" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--cirrus.svg'); ?>" alt="Cirrus" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--direct-debit.svg'); ?>" alt="Direct Debit" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--ebay.svg'); ?>" alt="Ebay" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--jcb.svg'); ?>" alt="JCB" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--maestro.svg'); ?>" alt="Maestro" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--mastercard-securecode.svg'); ?>" alt="Mastercard Securecode" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--mastercard.svg'); ?>" alt="Mastercard" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--norton.svg'); ?>" alt="Norton" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--paypal.svg'); ?>" alt="Paypal" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--sage.svg'); ?>" alt="Sage" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--stripe.svg'); ?>" alt="Stripe" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--verified-by-visa.svg'); ?>" alt="Verified By Visa" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--visa-electron.svg'); ?>" alt="Visa Electron" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--visa.svg'); ?>" alt="Visa" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--western-union.svg'); ?>" alt="Western Union" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/white--worldpay.svg'); ?>" alt="WordPay" width="63" height="40" /></li>
 </ul>
 
 <ul class="u-list-inline">
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--amazon.svg'); ?>" alt="Amazon" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--american-express.svg'); ?>" alt="America Express" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--braintree.svg'); ?>" alt="Braintree" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--cirrus.svg'); ?>" alt="Cirrus" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--direct-debit.svg'); ?>" alt="Direct Debit" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--ebay.svg'); ?>" alt="Ebay" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--jcb.svg'); ?>" alt="JCB" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--maestro.svg'); ?>" alt="Maestro" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--mastercard-securecode.svg'); ?>" alt="Mastercard Securecode" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--mastercard.svg'); ?>" alt="Mastercard" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--norton.svg'); ?>" alt="Norton" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--paypal.svg'); ?>" alt="Paypal" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--sage.svg'); ?>" alt="Sage" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--stripe.svg'); ?>" alt="Stripe" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--verified-by-visa.svg'); ?>" alt="Verified By Visa" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--visa-electron.svg'); ?>" alt="Visa Electron" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--visa.svg'); ?>" alt="Visa" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--western-union.svg'); ?>" alt="Western Union" width="63" height="40" /></li>
-    <li><img src="<?php echo getUrl('build/img/icons/intermediate-svg/payments--transparent--worldpay.svg'); ?>" alt="WordPay" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--amazon.svg'); ?>" alt="Amazon" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--american-express.svg'); ?>" alt="America Express" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--braintree.svg'); ?>" alt="Braintree" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--cirrus.svg'); ?>" alt="Cirrus" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--direct-debit.svg'); ?>" alt="Direct Debit" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--ebay.svg'); ?>" alt="Ebay" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--jcb.svg'); ?>" alt="JCB" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--maestro.svg'); ?>" alt="Maestro" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--mastercard-securecode.svg'); ?>" alt="Mastercard Securecode" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--mastercard.svg'); ?>" alt="Mastercard" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--norton.svg'); ?>" alt="Norton" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--paypal.svg'); ?>" alt="Paypal" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--sage.svg'); ?>" alt="Sage" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--stripe.svg'); ?>" alt="Stripe" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--verified-by-visa.svg'); ?>" alt="Verified By Visa" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--visa-electron.svg'); ?>" alt="Visa Electron" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--visa.svg'); ?>" alt="Visa" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--western-union.svg'); ?>" alt="Western Union" width="63" height="40" /></li>
+    <li><img src="<?php echo getUrl('build/img/icons/payments/svg/transparent--worldpay.svg'); ?>" alt="WordPay" width="63" height="40" /></li>
 </ul>
