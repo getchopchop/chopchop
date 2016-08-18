5ebad895112ca06b8574863126e377b0ac2e9b52
Gareth Joyce <gjoyce@iweb.co.uk>
2016-08-18 14:41:39 +0100

    #60 Added example of enquire.js and element-queries.js in by default but commented out.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,16 @@
+// =============================================
+// Set Breakpoints for Enquire
+// =============================================
+
+// var breakpoint = {
+//     xs: "screen and (min-width: 28em)",
+//     sm: "screen and (min-width: 35em)",
+//     md: "screen and (min-width: 48em)",
+//     lg: "screen and (min-width: 62em)",
+//     xl: "screen and (min-width: 82em)"
+// };
+
+
 ;(function($){
     $(document).ready(function() {
 
@@ -5,7 +18,8 @@
         // Global variables
         // =============================================
 
-        var $body = $('body');
+        var $body   = $('body');
+        var $window = $(window);
 
 
         // =============================================
@@ -26,6 +40,25 @@
         $('#overlay').on('chopchop:deactivate', function() {
             $body.removeClass('overlay-is-active');
         });
+
+
+        // =============================================
+        // Enquire Demo http://wicky.nillia.ms/enquire.js/
+        // =============================================
+
+        // enquire.register(breakpoint.md, {
+        //     setup : function() {
+        //         this.unmatch();
+        //     },
+        //     match: function () {
+        //         console.log('Upper')
+        //
+        //     },
+        //     unmatch: function () {
+        //         console.log('Lower');
+        //     }
+        // });
+
 
     });
 }(jQuery));
