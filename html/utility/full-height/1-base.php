9fc7b9922b59db11103a89521bfd8a774f4e96ca
Dave Redfern <dave@daveredfern.com>
2016-08-22 16:31:01 +0100

    #68 Fixing full height pattern

--- html/utility/full-height/1-base.php
+++ html/utility/full-height/1-base.php
@@ -3,15 +3,15 @@ Title: Full Height
 Class: .u-full-height
 */ ?>
 
-<div class="grid g-gutter u-full-height">
-    <div class="g-col-xs-6 u-fill-grey-light">
+<div class="grid g-gutter">
+    <div class="g-col-xs-6 u-fill-grey-light u-full-height">
         <div class="u-fill-grey-lighter">
             1<br>
             2<br>
             3
         </div>
     </div>
-    <div class="g-col-xs-6 u-fill-grey-light">
+    <div class="g-col-xs-6 u-fill-grey-light u-full-height">
         <div class="u-fill-grey-lighter">
             1
         </div>
