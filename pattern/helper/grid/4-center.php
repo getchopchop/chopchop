2facfe0bd2ed52e8f90be7721d7b46a64950f44d
Gareth Joyce <gjoyce@iweb.co.uk>
2016-11-07 16:14:15 +0000

    #125 added correct boxes to illustrate helper.

--- pattern/helper/grid/4-center.php
+++ pattern/helper/grid/4-center.php
@@ -4,5 +4,5 @@ Class: @include grid-center;
 */ ?>
 
 <div class="grid g-gutter">
-    <div class="g-col-xs-6"><div class="cc-box">Box</div></div>
+    <div class="g-col-xs-6"><div class="u-block u-fill-grey-lighter">Box</div></div>
 </div>
--- pattern/helper/grid/5-reverse.php
+++ pattern/helper/grid/5-reverse.php
@@ -4,6 +4,6 @@ Class: @include grid-center;
 */ ?>
 
 <div class="grid g-reverse">
-    <div class="g-col-xs-6"><div class="cc-box">1</div></div>
-    <div class="g-col-xs-6"><div class="cc-box">2</div></div>
+    <div class="g-col-xs-6"><div class="u-block u-fill-grey-lighter">1</div></div>
+    <div class="g-col-xs-6"><div class="u-block u-fill-grey-lighter">2</div></div>
 </div>
