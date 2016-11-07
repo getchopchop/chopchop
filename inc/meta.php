6faddc743dd6555e2d87bc8e9a8e956b77f163df
Gareth Joyce <gjoyce@iweb.co.uk>
2016-11-07 14:26:34 +0000

    #133 Move page includes into index.

--- inc/meta.php
+++
@@ -1,6 +0,0 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="mobile-web-app-capable" content="yes">
-<meta name="robots" content="noindex">
