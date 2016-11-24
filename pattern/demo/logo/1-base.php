67831b1139f5112d1e7391277ac8a0b0ae973bee
Gareth Joyce <gjoyce@iweb.co.uk>
2016-11-24 15:34:41 +0000

    #129 Moved logo into demo and remove sass.

---
+++ pattern/demo/logo/1-base.php
@@ -0,0 +1,8 @@
+<?php /*
+Title: Logo
+*/ ?>
+
+<a href="<?php echo getUrl(); ?>">
+    <img src="<?php echo getUrl('build/img/logos/logo.png'); ?>" width="100" height="100" />
+    <span class="u-visually-hidden">Site Name</span>
+</a>
