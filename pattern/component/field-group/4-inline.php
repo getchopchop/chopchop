7b7a76e761319e7f3be46be2b662291fe6d46c7a
Gareth Joyce <gjoyce@iweb.co.uk>
2016-12-15 14:12:54 +0000

    Fix #163

---
+++ pattern/component/field-group/4-inline.php
@@ -0,0 +1,12 @@
+<?php /*
+Title: Field Group Inline
+Class: .field-group.field-group--inline
+Description: Grids up at md, see component/field-group.scss
+*/ ?>
+
+<div class="field-group field-group--inline">
+    <label for="text" class="field-group__label required">Text Input</label>
+    <div class="field-group__field">
+        <input id="text" type="text" placeholder="Text Input">
+    </div>
+</div>
