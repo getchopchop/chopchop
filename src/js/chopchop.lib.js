7f654387b332d2bb3449b0e26e04aeffbfc4c7d2
Dave Baker <dbaker@iweb.co.uk>
2017-01-05 15:04:09 +0000

    Added group, target elements to be set

--- src/js/chopchop.lib.js
+++ src/js/chopchop.lib.js
@@ -54,8 +54,8 @@
 
         _createDataProperty: function(prop) {
             return this.options.dataPrepend + prop.replace(/^([a-z])/, function(a){
-                return a.toUpperCase();
-            });
+                    return a.toUpperCase();
+                });
         },
 
         // Constructs a global selector to be used, E.g. 'group' in the toggle
@@ -63,8 +63,8 @@
         _getGlobalDataSelector: function(prop, val) {
             val = val ? '="' + val + '"' : '';
             return '[data-' + this.options.dataPrepend.replace(/([A-Z])/g, function(a){
-                return "-" + a.toLowerCase();
-            }) + "-" + prop + val + ']';
+                    return "-" + a.toLowerCase();
+                }) + "-" + prop + val + ']';
         },
 
         _getData: function(prop) {
@@ -132,11 +132,22 @@
             targetCallback: '',
             stateful: true,
             target: '',
-            group: ''
         },
+
+        // Initialise as jQuery arrays in constructor
+        $targetElements: null,
+        $targetCallbackElements: null,
+        $cascadeElements: null,
+        $groupElements: null,
 
         _create: function(){
             this._super();
+
+            this.$targetElements = $([]);
+            this.$targetCallbackElements = $([]);
+            this.$cascadeElements = $([]);
+            this.$groupElements = $([]);
+
             this._addEvents();
         },
 
@@ -191,7 +202,19 @@
         },
 
         _getTargets: function(){
-            return $(this._getLocalOption(this.options.dataTarget), this._getClosestContainer());
+            return $(this._getLocalOption(this.options.dataTarget), this._getClosestContainer()).add(this.$targetElements);
+        },
+
+        _getTargetCallbackElements: function(){
+            return $(this._getLocalOption(this.options.dataTargetCallback), this._getClosestContainer()).add(this.$targetCallbackElements);
+        },
+
+        _getGroupItems: function(){
+            if(!this._getLocalOption(this.options.dataGroup)){
+                return this.$groupElements;
+            }
+
+            return $(this._getGlobalDataSelector(this.options.dataGroup, this._getLocalOption(this.options.dataGroup)), this._getClosestContainer()).add(this.$groupElements);
         },
 
         // Cascades can be used by data-cc-toggle-cascade, and can also be type based such
@@ -207,7 +230,7 @@
                 }
             }
 
-            return $(cascade, this._getClosestContainer());
+            return $(cascade, this._getClosestContainer()).add(this.$cascadeElements);
         },
 
         _getClosestContainer: function(){
@@ -278,6 +301,35 @@
             return this.element.hasClass(this.options.activeClass);
         },
 
+
+        addTargetElement: function($el){
+            this.$targetElements = this.$targetElements.add($el);
+        },
+
+        addGroupElements: function($els){
+            this.$groupElements = this.$groupElements.add($els);
+        },
+
+        removeTargetElement: function($el){
+            this.$targetElements = this.$targetElements.not($($el));
+        },
+
+        addCascadeElement: function($el){
+            this.$cascadeElements = this.$cascadeElements.add($($el));
+        },
+
+        removeCascadeElement: function($el){
+            this.$cascadeElements = this.$cascadeElements.not($($el));
+        },
+
+        addTargetCallbackElement: function($el){
+            this.$targetCallbackElements = this.$targetCallbackElements.add($($el));
+        },
+
+        removeTargetCallbackElement: function($el){
+            this.$targetCallbackElements = this.$targetCallbackElements.not($($el));
+        },
+
         destroy: function(){
             this._super();
             this._removeActiveClass();
@@ -312,8 +364,6 @@
                 type = this.isActive() ? Static.ACTION_DEACTIVATE : Static.ACTION_ACTIVATE;
             }
 
-
-
             if(type == Static.ACTION_ACTIVATE){
                 if(stateful) {
                     this._addActiveClass();
@@ -321,23 +371,20 @@
                 }
 
                 // Deal with groups
-                group = this._getLocalOption(this.options.dataGroup);
+                $(this._getGroupItems()).each(function(){
+                    var $this = $(this);
 
-                if(group){
-                    $(this._getGlobalDataSelector(this.options.dataGroup, group), this._getClosestContainer()).each(function(){
-                        var $this = $(this);
+                    // Don't do anything for *this* element, we're only interested in
+                    // deactivating other members of the group
+                    if($this.is(self.element)){
+                        return true;
+                    }
 
-                        // Don't do anything for *this* element, we're only interested in
-                        // deactivating other members of the group
-                        if($this.is(self.element)){
-                            return true;
-                        }
+                    // Don't cascade when deactivating group items otherwise targets
+                    // which should remain active will be deactivated
+                    self._getInstance($this).deactivate(true, false);
+                });
 
-                        // Don't cascade when deactivating group items otherwise targets
-                        // which should remain active will be deactivated
-                        self._getInstance($this).deactivate(true, false);
-                    });
-                }
             }else{
                 if(stateful) {
                     this._removeActiveClass();
@@ -358,8 +405,8 @@
 
             // data-cc-toggle-target-callback allows a selector to be chained, without it having its targets
             // or cascades also called. Most useful for group items to call back to a button or link
-            var $targetCallback = $(this._getLocalOption(this.options.dataTargetCallback), this._getClosestContainer());
-            
+            var $targetCallback = this._getTargetCallbackElements();
+
             $targetCallback.each(function(){
                 var $this = $(this);
 
@@ -370,6 +417,282 @@
         }
     });
 }(jQuery));
+
+
+// var ChopChop = (function($, ChopChop) {
+
+
+//
+// // Utilities
+// var ChopChop = (function($, ChopChop) {
+
+//
+//     return ChopChop;
+// })(jQuery, ChopChop || {});
+//
+// // Toggles
+// var ChopChop = (function($, ChopChop) {
+//     var defaultOptions = {
+//         classActive: 'is-active',
+//         classInactive: 'is-inactive'
+//     };
+//
+//     // Toggle
+//     var Toggle = ChopChop.Toggle = function(options) {
+//         this.options = $.extend({}, defaultOptions, options);
+//         this.pending = [];
+//         this.processed = [];
+//         this.init();
+//     };
+//
+//     var Action = {
+//         TOGGLE: 'toggle',
+//         ACTIVATE: 'activate',
+//         DEACTIVATE: 'deactivate'
+//     };
+//
+//     var Trigger = {
+//         DIRECT_ONLY: 'direct-only',
+//         INDIRECT_ONLY: 'indirect-only',     // Why would you want this? Oh well.
+//         BOTH: 'both'    // Default
+//     };
+//
+//     Toggle.prototype = {
+//         init: function() {
+//             var self = this;
+//
+//             $(document).on('click', '[data-cc-action]', function(e) {
+//                 var $this = $(this),
+//                     targets = $this.data('cc-target').split(' '),
+//                     mode = $this.data('cc-trigger-on') || Trigger.BOTH,
+//                     $target;
+//
+//                 for (var i = 0, l = targets.length; i < l; ++i) {
+//                     $target = $('#' + targets[i]);
+//
+//                     if ($target.size() === 0) {
+//                         continue;
+//                     }
+//
+//                     if (mode === Trigger.DIRECT_ONLY && e.target !== this) {
+//                         continue;
+//                     } else if (mode === Trigger.INDIRECT_ONLY && e.target === this) {
+//                         continue;
+//                     }
+//
+//                     e.preventDefault();
+//                     self.performAction($target, $this.data('cc-action') || Action.TOGGLE, $this);
+//                 }
+//             });
+//         },
+//         toggle: function(target, action) {
+//             var $target = typeof target === 'string' ? $('#' + target) : $(target);
+//
+//             if (!$target.size()) {
+//                 return;
+//             }
+//
+//             this.performAction($target, action || Action.TOGGLE);
+//         },
+//         cascade: function($el, action) {
+//             var i, l, $other,
+//                 chain = ($el.data('cc-cascade-' + action) || '').split(' ')
+//                         .concat(($el.data('cc-cascade') || '').split(' '));
+//
+//             for (i = 0, l = chain.length; i < l; ++i) {
+//                 $other = $('#' + chain[i]);
+//
+//                 if (this.processed.indexOf($other[0]) !== -1 || this.pending.indexOf($other[0]) !== -1) {
+//                     continue;
+//                 }
+//
+//                 this.pending.push({
+//                     action: action,
+//                     target: $other
+//                 });
+//             }
+//         },
+//         performAction: function($el, action, $source) {
+//             this.pending.length = 0;
+//             this.processed.length = 0;
+//
+//             if (action === Action.TOGGLE) {
+//                 if ($el.hasClass('is-active')) {
+//                     action = Action.DEACTIVATE;
+//                 } else {
+//                     action = Action.ACTIVATE;
+//                 }
+//             }
+//
+//             this.pending.push({
+//                 action: action,
+//                 target: $el
+//             });
+//
+//             var id, dependants, group, $current, item,
+//                 iterations = 0,
+//                 el, chain, $other, i;
+//
+//             while (this.pending.length > 0) {
+//                 item = this.pending.shift();
+//                 action = item.action;
+//                 $current = item.target;
+//
+//                 // Skip already processed nodes
+//                 if (this.processed.indexOf($current) !== -1) {
+//                     continue;
+//                 }
+//
+//                 id = $current.attr('id');
+//                 group = $current.data('cc-group');
+//
+//                 // Process group members, if a group is present
+//                 for (i = 0, all = $('[data-cc-group="' + group + '"]'), l = all.length; i < l; ++i) {
+//                     el = all[i];
+//                     $other = $(el);
+//
+//                     // Skip element itself
+//                     if (el === $current[0]) {
+//                         continue;
+//                     }
+//
+//                     // Just a group member, not the target: deactivate
+//                     $other.addClass('is-inactive').removeClass('is-active');
+//                     this.cascade($other, Action.DEACTIVATE);
+//                 }
+//
+//                 // Process target
+//                 if (action === Action.ACTIVATE) {
+//                     $current.addClass('is-active').removeClass('is-inactive');
+//                     $current.trigger('chopchop:activate', [$source]);
+//                 } else {
+//                     $current.addClass('is-inactive').removeClass('is-active');
+//                     $current.trigger('chopchop:deactivate', [$source]);
+//                 }
+//
+//                 // Chain onto targets
+//                 this.cascade($current, action);
+//                 this.processed.push($current[0]);
+//                 iterations += 1;
+//
+//                 if (iterations > 100) {
+//                     throw new ChopChop.Exception("Too many iterations, aborting");
+//                 }
+//             }
+//         }
+//     };
+//
+//     // Expose toggle api
+//     ChopChop.api.toggle = function() {
+//         return ChopChop.plugins.toggle.toggle.apply(ChopChop.plugins.toggle, arguments);
+//     };
+//
+//     return ChopChop;
+// })(jQuery, ChopChop || {});
+//
+// // Collapse
+// var ChopChop = (function($, ChopChop) {
+//     // Tabordion types
+//     var Types = {
+//         ONE_OPEN: 'one-open',
+//         ONE_COLLAPSIBLE: 'one-collapsible',
+//         ALL_COLLAPSIBLE: 'all-collapsible'
+//     };
+//
+//     var defaultOptions = {
+//         classActive: 'is-active',
+//         classInactive: 'is-inactive',
+//         headerSelector: '> [class*="__header"]',
+//         bodySelector: '> [class*="__body"]',
+//         type: Types.ONE_OPEN
+//     };
+//
+//     // Collapse
+//     var Collapse = ChopChop.Collapse = function(options) {
+//         this.options = $.extend({}, defaultOptions, options);
+//         this.id = 0;
+//         this.init();
+//     };
+//
+//     // Attribute creators and appliers for all collapse types
+//     var attributors = {};
+//     attributors[Types.ONE_OPEN] = function($header, $body, name, i) {
+//         $header.attr({
+//             'id': name + '-header-' + i,
+//             'data-cc-group': name + '-headers',
+//             'data-cc-action': 'activate',
+//             'data-cc-target': name + '-body-' + i
+//         });
+//         $body.attr({
+//             'id': name + '-body-' + i,
+//             'data-cc-group': name + '-bodies',
+//             'data-cc-cascade-activate': name + '-header-' + i
+//         });
+//     };
+//     attributors[Types.ONE_COLLAPSIBLE] = function($header, $body, name, i) {
+//         $header.attr({
+//             'id': name + '-header-' + i,
+//             'data-cc-group': name + '-headers',
+//             'data-cc-action': 'toggle',
+//             'data-cc-target': name + '-body-' + i
+//         });
+//         $body.attr({
+//             'id': name + '-body-' + i,
+//             'data-cc-group': name + '-bodies',
+//             'data-cc-cascade': name + '-header-' + i
+//         });
+//     };
+//     attributors[Types.ALL_COLLAPSIBLE] = function($header, $body, name, i) {
+//         $header.attr({
+//             'id': name + '-header-' + i,
+//             'data-cc-action': 'toggle',
+//             'data-cc-target': name + '-body-' + i
+//         });
+//         $body.attr({
+//             'id': name + '-body-' + i,
+//             'data-cc-cascade': name + '-header-' + i
+//         });
+//     };
+//
+//     Collapse.prototype = {
+//         init: function() {
+//             var self = this;
+//
+//             $('[data-cc-collapse]').each(function() {
+//                 self.applyToContainer(this);
+//             });
+//         },
+//         getNextName: function() {
+//             return 'collapse' + (++this.id);
+//         },
+//         applyToContainer: function(root, options) {
+//             options = $.extend({}, this.options, options);
+//
+//             var $root = $(root),
+//                 name = options.name || $root.data('cc-collapse-name') || this.getNextName(),
+//                 type =  $root.data('cc-collapse') || options.type,
+//                 headers = $root.find(options.headerSelector),
+//                 bodies = $root.find(options.bodySelector);
+//
+//             if (headers.size() !== bodies.size()) {
+//                 throw new ChopChop.Exception(
+//                     "Collapse: number of headers does not match number of bodies (" + headers.size() + " vs " + bodies.size()+ ")"
+//                 );
+//             }
+//
+//             for (var i = 0, l = headers.size(); i < l; ++i) {
+//                 attributors[type]($(headers[i]), $(bodies[i]), name, i);
+//             }
+//         }
+//     };
+//
+//     // Expose api
+//     ChopChop.api.collapse = function() {
+//         return ChopChop.plugins.collapse.applyToContainer.apply(ChopChop.plugins.collapse, arguments);
+//     };
+//
+//     return ChopChop;
+// })(jQuery, ChopChop || {});
 
 
 // var ChopChop = (function($, ChopChop) {
