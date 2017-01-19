9e1ca745171a6c11df8ce0156aa220a2862fece8
Dave Baker <dbaker@iweb.co.uk>
2017-01-19 15:26:13 +0000

    Alerts

--- pattern/component/alert/2-dismissible.php
+++ pattern/component/alert/2-dismissible.php
@@ -4,5 +4,5 @@ Class: .alert.alert--dismissible
 */ ?>
 
 <div class="alert alert--dismissible" id="alert-dismissible">
-    <p>This is a default alert message with a close option. <a href="#" class="close" data-cc-action="deactivate" data-cc-target="alert-dismissible">Close</a></p>
+    <p>This is a default alert message with a close option. <a href="#" class="close" data-cc-toggle-action="deactivate" data-cc-toggle-target="#alert-dismissible">Close</a></p>
 </div>
