9a9408d262b355067357b6d7a0abdfcc8fd52f0c
Dave Redfern <dave@daveredfern.com>
2017-01-20 09:15:40 +0000

    Close #173 Adding fixed and fluid gutter

---
+++ pattern/utility/grid/4-gutter-x-lg.php
@@ -0,0 +1,15 @@
+<?php /*
+Title: Grid Gutter X Large
+Class: .g-gutter-x-lg
+*/ ?>
+
+<div class="grid g-gutter-x-lg">
+    <div class="g-col-xs-3"><div class="u-block u-fill-neutral-lighter"><code>.g-col-xs-3</code></div></div>
+    <div class="g-col-xs-3"><div class="u-block u-fill-neutral-lighter"><code>.g-col-xs-3</code></div></div>
+    <div class="g-col-xs-3"><div class="u-block u-fill-neutral-lighter"><code>.g-col-xs-3</code></div></div>
+    <div class="g-col-xs-3"><div class="u-block u-fill-neutral-lighter"><code>.g-col-xs-3</code></div></div>
+    <div class="g-col-xs-3"><div class="u-block u-fill-neutral-lighter"><code>.g-col-xs-3</code></div></div>
+    <div class="g-col-xs-3"><div class="u-block u-fill-neutral-lighter"><code>.g-col-xs-3</code></div></div>
+    <div class="g-col-xs-3"><div class="u-block u-fill-neutral-lighter"><code>.g-col-xs-3</code></div></div>
+    <div class="g-col-xs-3"><div class="u-block u-fill-neutral-lighter"><code>.g-col-xs-3</code></div></div>
+</div>
