4824d51a58e16f967d018391fe0aa601581350eb
Gareth Joyce <gjoyce@iweb.co.uk>
2017-02-28 13:46:43 +0000

    Tidy up after #194

--- pattern/demo/toggle/8-grouped-toggles.php
+++ pattern/demo/toggle/8-grouped-toggles.php
@@ -1,21 +1,18 @@
 <?php /*
-Title: Grouped Toggles
+Title: Grouped Toggles with button demo
+Description: Shows an example of it working with a read more/read less button where the button text changes.
 */ ?>
 
 
-
-
-
-<div class="u-toggle-xs u-toggle-sm u-toggle is-active" id="full-description">
-    🏴‍☠️
+<div class="u-toggle" id="full-description">
+    <h1>🏴‍☠️</h1>
 </div>
 
-
-<p class="u-text-center u-toggle is-inactive cc-toggle" id="full-description-show" data-cc-toggle-group="full-description-buttons">
-    <a href="#" class="btn is-active cc-toggle" data-cc-toggle-target="#full-description, #full-description-hide" data-cc-toggle-action="toggle">Read full description</a>
+<p class="u-toggle is-active" id="full-description-show" data-cc-toggle-group="full-description-buttons">
+    <a href="#" class="is-active" data-cc-toggle-target="#full-description, #full-description-hide" data-cc-toggle-action="toggle">Show Pirate</a>
 </p>
 
-<p class="u-text-center u-toggle is-active cc-toggle" id="full-description-hide" data-cc-toggle-group="full-description-buttons">
-    <a href="#" class="btn is-inactive cc-toggle" data-cc-toggle-target="#full-description, #full-description-show" data-cc-toggle-action="toggle">Close full description</a>
+<p class="u-toggle" id="full-description-hide" data-cc-toggle-group="full-description-buttons">
+    <a href="#" data-cc-toggle-target="#full-description, #full-description-show" data-cc-toggle-action="toggle">Hide Pirate</a>
 </p>
 
