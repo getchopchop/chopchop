e7e6ab8126d2c750462b3c79abf7309809addb32
Dave Redfern <dredfern@iweb.co.uk>
2017-03-07 14:11:02 +0000

    Deleting nav check

--- pattern/component/nav/check.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul class="nav nav--check">
-        <li><a href="#">Overview</a></li>
-        <li><a href="#">Credits</a></li>
-        <li><a href="#">Structure</a></li>
-    </ul>
-</nav>
