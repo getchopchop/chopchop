4e1feaa4fc5374c8792591077d082e275a51fd54
Gareth Joyce <gjoyce@iweb.co.uk>
2017-03-08 16:15:36 +0000

    Bring in the kitchen sink js.

---
+++ kitchen-sink.php
@@ -0,0 +1,7 @@
+<script src="<?php echo getUrl('build/vendor/llapgoch/melodeon.widget.js'); ?>"></script>
+<script src="<?php echo getUrl('build/vendor/enquire/enquire.js'); ?>"></script>
+<script src="<?php echo getUrl('build/js/chop.enquire.breakpoint.js'); ?>"></script>
+<script src="<?php echo getUrl('build/js/chop.scroll.js'); ?>"></script>
+<script src="<?php echo getUrl('build/js/chop.listing.js'); ?>"></script>
+<script src="<?php echo getUrl('build/js/chop.product.js'); ?>"></script>
+<script src="<?php echo getUrl('build/js/script.js'); ?>"></script>
