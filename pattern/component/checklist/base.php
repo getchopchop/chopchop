9338fa47f5c080302e85ebdca2e2de20665345c7
Gareth Joyce <gjoyce@iweb.co.uk>
2017-03-09 08:35:07 +0000

    Remove line from checklist.

--- pattern/component/checklist/base.php
+++ pattern/component/checklist/base.php
@@ -8,7 +8,6 @@ Class: .checklist
     <li><a href="#">Integral</a></li>
     <li><a href="#">Kingston</a></li>
     <li class="is-active"><a href="#">Lexar</a></li>
-    <li><a href="#">MyMemory</a></li>
     <li><a href="#">Samsung</a></li>
     <li><a href="#">SanDisk</a></li>
     <li><a href="#">Toshiba</a></li>
